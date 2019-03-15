03bc44f3f5385904b0cc53c92cfa28735ee9462e
Anthony <anthonysarkis@gmail.com>
2019-03-14 17:47:56 -0700

    WIP, New, predict_from_url() method
    -> "all in one" method to handle prediction for a new file.

--- sdk/diffgram/brain/brain.py
+++ sdk/diffgram/brain/brain.py
@@ -1,10 +1,14 @@
+from copy import deepcopy
 
 
 class Brain():
 
 	def __init__(self, client):
 
-		self.client = client
+		# If someone calls auth on client
+		# We don't want existing models effected.
+
+		self.client = deepcopy(client)
 
 		if self.client.project_string_id is None:
 			raise Exception("\n Client must be authenticated use client.auth()")
@@ -15,6 +19,9 @@ class Brain():
 
 	def predict(self, file):
 		"""
+
+		WIP for passing file object
+		in case where diffgram already has file.
 
 		returns an inference object
 		"""
@@ -31,4 +38,34 @@ class Brain():
 		data = response.json()
 
 		print(data)
+
+
+	def predict_from_url(
+			self,
+			url):
+
+		request = {}
+		request['url'] = url
+		request['ai_name'] = self.name
+
+		endpoint = "/api/v1/project/" + self.client.project_string_id + \
+			"/inference/from_url"
+
+		response = self.client.session.post(
+			self.client.host + endpoint, 
+			json = request)
+
+		self.client.handle_errors(response)
+
+		data = response.json()
+
+		# TODO create Inference() object...		
+		# How we want to return / work with that
+
+		self.client.handle_errors(response)
+
+		if data["log"]["success"] is True:
+			pass
+
+		return None
 
