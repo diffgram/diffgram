a4f0ad5eef0f378e10e4373b7267fca3f6193002
Anthony Sarkis <swirlingsand@users.noreply.github.com>
2020-02-03 14:42:40 -0800

    Add a 'new' from dict method
    use new regular refresh_from_dict

--- sdk/diffgram/file/file.py
+++ sdk/diffgram/file/file.py
@@ -1,5 +1,13 @@
+from ..regular.regular import refresh_from_dict
+
 
 class File():
+	"""
+	file literal object
+	See File Constructor for creating new files in Diffgram Service
+	
+	Feb 3, 2020. Perhaps should be all in the same File class.
+	"""
 
 
 	def __init__(
@@ -9,13 +17,41 @@ class File():
 		self.id = id
 
 
-	def something():
-		pass
+	def new(file_json):
+		"""
+		New is new object from *Dict*
+		get_xxx methods are for getting object 
+		from Diffgram Service.
+
+		In the current context a user doesn't create a new 
+		File directly, the system creates a file at import
+		and/or when copying / operating on a file.
+
+		Could also call this new_from_dict()?
+
+		Feb 3, 2020
+			For now this is following pattern as in 
+			Export class
+
+		"""
+		
+		file = File()
+		refresh_from_dict(file, file_json)
+		return file
+
 
 	def serialize(self):
 
 		return {
 			'id' : self.id
 			}
+
+
+	def get_by_id(self, id: int):
+		"""
+		WIP for refreshing from server side.
+		"""
+
+		raise NotImplemented
 
 
