a2ea84d465243885a7bf880f1de5ff7405640ca3
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-04-13 16:13:10 -0700

    The SDK has moved
    https://github.com/diffgram/python-sdk

--- samples/settings.py
+++
@@ -1,3 +0,0 @@
-CLIENT_ID = "replace"
-CLIENT_SECRET = "replace"
-PROJECT_STRING_ID = "replace"
--- sdk/diffgram/__init__.py
+++
@@ -1,6 +0,0 @@
-__name__ = "diffgram"
-__version__ = "0.3.3"
-
-from diffgram.core.core import Project
-from diffgram.file.file import File
-from diffgram.task.task import Task
