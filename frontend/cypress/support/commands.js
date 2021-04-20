1fba681cb70ff35902e5031be2e29f31a9bd86ed
Pablo <pjestradac@gmail.com>
2021-04-19 20:39:50 -0600

    Add time to remove chunk load error

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -330,7 +330,7 @@ Cypress.Commands.add('loginByForm', function (email, password) {
 
 Cypress.Commands.add('gotToProject', function (project_string_id) {
   cy.visit('http://localhost:8085/projects')
-  cy.wait(5000);
+  cy.wait(8000);
   cy.get(`[data-cy="project-title-${project_string_id}"] > div`).click({force: true});
   cy.wait(3000);
 });
