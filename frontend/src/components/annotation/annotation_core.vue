c88c6248f77d7b48cc42cee1219de1bde7654d81
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-05-14 13:11:32 -0700

    add id guard too in context of needing this for hotreload

--- frontend/src/components/annotation/annotation_core.vue
+++ frontend/src/components/annotation/annotation_core.vue
@@ -5446,6 +5446,7 @@ export default Vue.extend( {
     },
     on_change_current_task: async function(){
       if (!this.$props.task) { return }
+      if (!this.$props.task.id) { return }
 
       if (this.loading == true || this.annotations_loading == true || this.full_file_loading) {
         return
@@ -5465,6 +5466,7 @@ export default Vue.extend( {
     },
     on_change_current_file: async function () {
       if (!this.$props.file) { return }
+      if (!this.$props.file.id) { return }
 
       if (this.loading == true || this.annotations_loading == true || this.full_file_loading) {
         // Don't change file while loading
