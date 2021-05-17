8f8b629d606858861b8e12e6a11b7928a7089de0
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-05-17 16:31:19 -0700

    refactor: hotkeys into own component for faster updates
    better structure

--- frontend/src/components/annotation/toolbar.vue
+++ frontend/src/components/annotation/toolbar.vue
@@ -382,7 +382,7 @@
 
   </button_with_menu>
 
-    <button_with_menu
+  <button_with_menu
     tooltip_message="Hotkeys"
     v-if="view_only_mode != true"
     color="primary"
@@ -391,52 +391,7 @@
         >
 
     <template slot="content">
-      <v-layout column>
-
-        <h2> General </h2>
-
-        Right click an instance to bring up context menu.
-
-        <p> <kbd>Esc</kbd> Toggle draw and edit mode </p>
-
-        <p> <kbd>Esc</kbd> (Twice) Cancel current drawing and return to draw mode </p>
-
-        <p> <kbd>W</kbd> Toggle Label Menu </p>
-        <p> <kbd>1 - 9</kbd> Change label </p>
-
-        <p> <kbd>Shift</kbd> + <kbd>←</kbd>,<kbd>→</kbd> Previous or Next File </p>
-        <p> <kbd>Ctrl</kbd> + <kbd>c</kbd> Copy Selected Instance </p>
-        <p> <kbd>Ctrl</kbd> + <kbd>v</kbd> Paste Selected Instance </p>
-
-        <p> <kbd>C</kbd> Complete. Save, Mark as Complete, Go to Next.</p>
-
-        <h2> Video </h2>
-
-        <p> <kbd>Spacebar</kbd> Play/Pause Video </p>
-
-        <p> <kbd>←</kbd>,<kbd>→</kbd> or <kbd>A</kbd>, <kbd>D</kbd> Previous or Next Frame</p>
-
-        <p> <kbd>F</kbd> New Sequence </p>
-        <p> <kbd>Shift</kbd> + <kbd>n</kbd> Jump to Next Instance </p>
-        <h2> Image / Frame </h2>
-        <p> <kbd>S</kbd> Save </p>
-
-        <p> <kbd>Delete</kbd> Deletes selected instances </p>
-
-        <p> <kbd>Mouse wheel</kbd> Zoom / pan </p>
-
-        <p> <kbd>Ctrl</kbd> Pan Only </p>
-
-        <h2> Polygons </h2>
-        <p> <kbd>Enter</kbd> Complete polygon (Or click first point again)  </p>
-
-        <p> <b>Hold </b> <kbd>Shift</kbd>
-          🔥 Turbo mode, auto places point as you move mouse.
-          <br />
-          Can switch between this mode and normal as needed by
-          releasing shift.</p>
-
-      </v-layout>
+      <hotkeys></hotkeys>
     </template>
 
   </button_with_menu>
@@ -797,6 +752,7 @@ import file_meta_data_card from './file_meta_data_card'
 import task_relations_card from './task_relations_card'
 import file_relations_card from './file_relations_card'
 import task_meta_data_card from './task_meta_data_card'
+import hotkeys from './hotkeys'
 
 export default Vue.extend( {
 
@@ -807,6 +763,7 @@ export default Vue.extend( {
     file_relations_card,
     task_meta_data_card,
     task_relations_card,
+    hotkeys
   },
   props: {
     'project_string_id': {
