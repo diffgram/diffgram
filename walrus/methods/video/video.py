88fac31f4404ffeacc2994df1f596b00ac72f7e5
Pablo <pjestradac@gmail.com>
2021-06-02 14:05:47 -0600

    fix: indents

--- walrus/methods/video/video.py
+++ walrus/methods/video/video.py
@@ -357,9 +357,10 @@ class New_video():
         """
 
         if self.input.frame_packet_map == {}:
+            return
 
-
-        if self.check_update_log_errors() is False: return
+        if self.check_update_log_errors() is False:
+            return
 
         self.try_to_commit()  # 1/2 needed otherwise foreign key violation because
         # 2/2 new sequences won't exist yet
