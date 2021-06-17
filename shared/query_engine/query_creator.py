ca45b57e5e0f8dae02f7244aa1b7c19731542c50
Pablo <pjestradac@gmail.com>
2021-06-17 16:54:05 -0600

    wip: implemented tree visitor and basic query suggestions.

--- shared/query_engine/query_creator.py
+++ shared/query_engine/query_creator.py
@@ -3,103 +3,61 @@ from shared.helpers.sessionMaker import session_factory
 from lark import Lark, Transformer, v_args
 from lark.exceptions import UnexpectedCharacters, UnexpectedToken
 from sqlalchemy import func
+from shared.query_engine.diffgram_query import DiffgramQuery
+from shared.query_engine.grammar import grammar_definition
 
 
 @v_args(inline = True)  # Affects the signatures of the methods
 class DiffgramQueryProcessor(Transformer):
-    conditions = []
-
-    def expr(self, *args):
-        print('ON EXPR', args)
-
-    def start(self, *args):
-        print('ON start', args)
-        return
-
-    def factor(self, *args):
-        print('ON factor', args)
-
-    def term(self, *args):
-        print('ON term', args)
-
-    def compare_op(self):
-        print('ON compare_op')
-
-    def compare_expr(self, *args):
-        print('on compare_expr', args)
-        self.conditions.append(
-            args
-        )
-
-
-def show_next_possible_tokens(error):
-    print('error', error)
-    # print('ERROR HANDLER  :) TOKEN', error.interactive_parser)
-    return True
+    conditions = {'root': []}
+    current_node = None
 
 
 class QueryCreator:
     """
         Responsible for creating a DiffgramQuery based on the defined syntax.
     """
-    grammar_definition = """
-        start: expr
-        expr: term [OR term]
-        term: factor [AND factor]
-        factor: compare_expr
-        compare_expr: NAME COMPARE_OP NAME
-        COMPARE_OP: ">" | "<" | "=" | "!=" | ">=" | "<="
-        OR: "or"
-        AND: "and"
-        NAME: CNAME [DOT CNAME]+ | CNAME | NUMBER
-        DOT: "."
-        %import common.CNAME 
-        %import common.NUMBER
-        _WHITESPACE: /[ \t]+/ 
-        %ignore _WHITESPACE
-    """
 
     def __init__(self, session, project):
         self.project = project
         self.session = session
-        self.parser = query_parser = Lark(self.grammar_definition,
+        self.parser = query_parser = Lark(grammar_definition,
                                           parser = 'lalr',
-                                          transformer = DiffgramQueryProcessor())
+                                          transformer = None)
         self.build_query = query_parser.parse
 
     def get_suggestions(self, query_string):
         if query_string == '':
-            return ['annotations', 'labels', 'file', 'instance', 'dataset', 'issues']
+            return ['labels', 'file', 'instance', 'dataset', 'issues']
         try:
             result = self.create_query(query_string)
-            return result
+            # If the query is correct we don't suggest anything
+            return []
         except UnexpectedCharacters as chars_exception:
-            print('UnexpectedCharacters',chars_exception)
+            if type(chars_exception) == UnexpectedCharacters:
+                if chars_exception.char == '.':
+                    last_token = chars_exception.state.value_stack[0]
+                    suggestions = []
+                    if last_token.value in ['label', 'labels']:
+                        # Show available labels in project.
+                        labels = self.project.get_label_list(self.session, directory = self.project.directory_default)
+                        for label in labels:
+                            suggestions.append(label['label']['name'])
+                    elif last_token.value == ['file', 'files']:
+                        suggestions = ['date', 'metadata', 'tag']
+                    elif last_token.value == ['instance', 'instances']:
+                        suggestions = ['type', 'count', 'tag', 'model', 'model_run']
+                    elif last_token.value == ['issues', 'issue']:
+                        suggestions = ['status', 'count']
+                    return suggestions
+            return []
         except UnexpectedToken as token_exception:
-            last_char_index = token_exception.state.state_stack[0]
-            print('LAST index: ', last_char_index)
-            print('LAST CHAR: ', query_string[last_char_index])
-            print('token: ', token_exception.token)
-            print('char: ', token_exception.char)
-            print('line: ', token_exception.line)
-            print('_terminals_by_name: ', token_exception._terminals_by_name)
-            print('column: ', token_exception.column)
-            print('accepts: ', token_exception.accepts)
-            print('state: ', token_exception.state)
-            print('state_stack: ', token_exception.state.state_stack)
-            print('state: ', token_exception.state.value_stack)
-            print('expected: ', token_exception.expected)
-            print('considered_rules: ', token_exception.considered_rules)
-            print('interactive_parser: ', token_exception.interactive_parser)
-            print('interactive_parser accepts: ', token_exception.interactive_parser.accepts())
-            print('interactive_parser choices: ', token_exception.interactive_parser.choices())
-            print('interactive_parser choices: ', token_exception.interactive_parser.choices())
-            print('_terminals_by_name: ', token_exception._terminals_by_name)
-            print('token_history: ', token_exception.token_history)
-
+            print('UNEXPECTED TOKEN')
+            return
 
     def create_query(self, query_string):
-        return self.build_query(query_string, on_error = show_next_possible_tokens)
+        tree = self.build_query(query_string)
+        return DiffgramQuery(tree)
 
 
 def get_files_by_instance_count(session):
