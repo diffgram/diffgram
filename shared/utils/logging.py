00430d10f64a6d2fda48a1bffae9bcab11fa1f06
Pablo <pjestradac@gmail.com>
2021-06-25 11:58:06 -0600

    fix: remove gcp logger completely
    We should not assume any provider on the logging context. It's going to be better to add more configurations to support external logging integrations in the future.

--- shared/utils/logging.py
+++ shared/utils/logging.py
@@ -49,11 +49,7 @@ class DiffgramLogger:
             settings.RUNNING_LOCALLY == True:
             self.logger = self.configure_sandbox_testing_logger()
         elif system_mode == 'production':
-            # self.logger = self.configure_sandbox_testing_logger()
-            try:
-                self.logger = self.configure_gcp_logger()
-            except:
-                self.logger = self.configure_sandbox_testing_logger()
+            self.logger = self.configure_sandbox_testing_logger()
 
         if self.logger is None and not DiffgramLogger.logging_initialized.get(self.logger_name):
             # Default to always create a logger, eg in case of some settings being mis-configured
