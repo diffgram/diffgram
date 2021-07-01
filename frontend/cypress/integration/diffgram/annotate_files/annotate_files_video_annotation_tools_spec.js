c7e86e6e300e50595108018ed0ec8d5b34fef6c7
Pablo <pjestradac@gmail.com>
2021-06-30 22:35:57 -0600

    fix: e2e test

--- frontend/cypress/integration/diffgram/annotate_files/annotate_files_video_annotation_tools_spec.js
+++ frontend/cypress/integration/diffgram/annotate_files/annotate_files_video_annotation_tools_spec.js
@@ -91,12 +91,13 @@ describe('Annotate Files Tests', () => {
 
       it('Shows Ghost Instances', () => {
         cy.get('[data-cy="edit_toggle"]').click({force: true})
-        cy.get('[data-cy="edit_toggle"]').parent().parent().find('label').should('have.text', 'Drawing')
-        cy.wait(1000)
-        cy.get('[data-cy="forward_1_frame"]').click({force: true})
-        cy.wait(1000)
-        cy.window().then(window => {
-
+        .wait(1000)
+        .get('[data-cy="edit_toggle"]').parent().parent().find('label').should('have.text', 'Drawing')
+        .wait(1000)
+        .get('[data-cy="forward_1_frame"]').click({force: true})
+        .wait(2000)
+        .window().then(window => {
+          cy.wait(2000)
           expect(window.AnnotationCore.ghost_instance_list.length).to.be.at.least(1);
 
         });
