e9c7fa6a8e25694b3852b5957a2e5204524d6c38
Pablo <pjestradac@gmail.com>
2021-07-01 23:57:29 -0600

    feat: added chunking

--- default/main.py
+++ default/main.py
@@ -69,6 +69,18 @@ def log_request_info():
 		print("Failed to log ", e)
 
 
+@app.before_request
+def handle_chunking():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
+
 @app.after_request
 def apply_security_rules(response):
 	response.headers["X-Frame-Options"] = "SAMEORIGIN"
