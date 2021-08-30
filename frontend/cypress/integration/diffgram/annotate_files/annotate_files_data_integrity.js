64bece845c0459fc8af837afd97889f23ab86ee4
PJEstrada <pjestrada006@hotmail.com>
2021-08-30 11:02:34 -0700

    Use Same Frame value and remove soft_delete on hash collisions (#265)
    * fix: hashes and frame changes to prevent box deletions
    
    * use same var name
    
    * prevent serializing 2 instances with same ID
    
    * small optimization
    
    * fix: dont remove old_id
    
    * wip: save frames in parallel for paste on next frames
    
    * remove timeout of paste message
    
    * add error component for paste multiple frames
    
    * wip: adding test cases
    
    * cleanup
    
    * wip: unit tests
    
    * tests: add unit tests for overlap
    
    * test: annotation update unit tests
    
    * fix: e2e tests
    
    Co-authored-by: Pablo <pjestradac@gmail.com>

--- frontend/cypress/integration/diffgram/annotate_files/annotate_files_data_integrity.js
+++ frontend/cypress/integration/diffgram/annotate_files/annotate_files_data_integrity.js
@@ -14,7 +14,7 @@ describe('Annotate Files Tests', () => {
       cy.loginByForm(testUser.email, testUser.password);
       cy.gotToProject(testUser.project_string_id);
       cy.createLabels(testLabels)
-      cy.uploadAndViewSampleImage(testUser.project_string_id);
+      cy.uploadAndViewSampleVideo(testUser.project_string_id);
 
     })
 
@@ -108,6 +108,62 @@ describe('Annotate Files Tests', () => {
       })
     })
 
+    context('It Correctly saves in parellel all frames when pasting to multiple frames', () => {
+      it('Correctly raises an instance_list integrity error.', () => {
+        cy.intercept(`api/project/*/file/*/annotation/update`).as('annotation_update')
+
+
+        // Draw 1 boxes
+        const boxes = [
+          {
+            min_x: 88,
+            min_y: 88,
+            max_x: 166,
+            max_y: 166,
+          },
+        ]
+        for (let box of boxes) {
+          cy.mousedowncanvas(box.min_x, box.min_x)
+          .wait(500)
+
+          .mouseupcanvas()
+          .wait(1000)
+
+          .mousedowncanvas(box.max_x, box.max_x)
+          .wait(500)
+          .mouseupcanvas()
+
+          .wait(2000)
+        }
+        cy.wait(7000)
+          .get('[data-cy="edit_toggle"]').click({force: true})
+          .mousedowncanvas(90, 90)
+          .wait(500)
+          .mouseupcanvas()
+          .wait(1000)
+          .rightclickdowncanvas(90, 90)
+          .wait(1000)
+          .get('[data-cy=copy_instance]').should('exist')
+          .get('[data-cy=copy_instance]').click({force: true})
+          .wait(1000)
+          .rightclickdowncanvas(90, 90)
+          .get('[data-cy=show_menu_paste_next_frames]').click({force: true})
+          .wait(500)
+          .get('[data-cy=paste_frame_count').type('{backspace}5')
+          .get('[data-cy=paste_next_frames]').click({force: true})
+          .wait(500)
+          .wait(10000)
+          .get('@annotation_update.all').should('have.length', 6)
+          .then((xhrs) => {
+            expect(xhrs[0].response, 'request status').to.have.property('statusCode', 200)
+            expect(xhrs[1].response, 'request status').to.have.property('statusCode', 200)
+            expect(xhrs[2].response, 'request status').to.have.property('statusCode', 200)
+            expect(xhrs[3].response, 'request status').to.have.property('statusCode', 200)
+            expect(xhrs[4].response, 'request status').to.have.property('statusCode', 200)
+            expect(xhrs[5].response, 'request status').to.have.property('statusCode', 200)
+          })
+      });
+    })
   })
 
 })
