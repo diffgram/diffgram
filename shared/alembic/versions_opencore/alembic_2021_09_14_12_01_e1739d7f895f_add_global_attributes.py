f4cefa589d4b6ee75d3f2b5a29fe322a09b8fffa
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-09-14 20:28:01 -0700

    Create alembic_2021_09_14_12_01_e1739d7f895f_add_global_attributes.py

---
+++ shared/alembic/versions_opencore/alembic_2021_09_14_12_01_e1739d7f895f_add_global_attributes.py
@@ -0,0 +1,27 @@
+"""add global attributes
+
+Revision ID: e1739d7f895f
+Revises: 715370463da5
+Create Date: 2021-09-14 12:01:53.654617
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e1739d7f895f'
+down_revision = '715370463da5'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('attribute_template_group', 
+                        sa.Column('is_global', sa.Boolean, default = False))
+    op.add_column('attribute_template_group', 
+                        sa.Column('global_type', sa.String, default = 'file'))
+
+def downgrade():
+    op.drop_column('attribute_template_group', 'is_global')
+    op.drop_column('attribute_template_group', 'global_type')
