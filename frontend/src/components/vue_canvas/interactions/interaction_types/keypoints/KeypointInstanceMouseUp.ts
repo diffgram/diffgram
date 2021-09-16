614c8812b3f031734ef6ae040a1368cacdfc18d6
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-09-16 13:24:35 -0700

    refactor to more global stop concept for more clear adding of new ones

--- frontend/src/components/vue_canvas/interactions/interaction_types/keypoints/KeypointInstanceMouseUp.ts
+++ frontend/src/components/vue_canvas/interactions/interaction_types/keypoints/KeypointInstanceMouseUp.ts
@@ -17,24 +17,7 @@ export class KeypointInstanceMouseUp extends Interaction{
     * Handles the mouse up event for the keypoint instance by adding a node
     * selecting, or finishing a drag depending on the instances's state.
     * */
-    if (this.key_point_instance.instance_context.draw_mode
-      && this.key_point_instance.template_creation_mode) {
-      this.key_point_instance.add_node_to_instance();
-    } else {
-      if(this.key_point_instance.is_moving){
-        this.key_point_instance.stop_moving();
-      }
-
-      if(this.key_point_instance.node_hover_index != undefined){
-        this.key_point_instance.select();
-      }
-
-      if(this.key_point_instance.is_dragging_instance){
-        this.key_point_instance.stop_dragging()
-      }
-
-    }
-
+    this.key_point_instance.stop()
   }
 
 }
