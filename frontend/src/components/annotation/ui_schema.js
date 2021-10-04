d57680ec48abb720d70024f5545ffea47bfb4218
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-10-04 14:54:28 -0700

    work in progress

---
+++ frontend/src/components/annotation/ui_schema.js
@@ -0,0 +1,40 @@
+
+
+
+export class UI_Schema {
+
+  constructor() {
+    this.show_logo = false
+  }
+
+  undo(){
+    const command = this.command_history[this.command_index];
+    if(!command){return}
+    command.undo();
+    if(this.command_index != undefined && this.command_index > 0){
+      this.command_index -= 1;
+    }
+    else if(this.command_index != undefined && this.command_index === 0){
+      this.command_index = undefined;
+    }
+    return true
+  }
+
+  redo(){
+    if(this.command_index == undefined && this.command_history.length > 0){
+      this.command_index = 0;
+      const command = this.command_history[this.command_index]
+      if(!command){return}
+      command.execute();
+    }
+    else if(this.command_index >= 0){
+      this.command_index += 1;
+      const command = this.command_history[this.command_index]
+      if(!command){return}
+      command.execute();
+    }
+    return true
+  }
+
+}
+
