0543c152420e5cf9c443481b9de728ebf2f06b25
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-10-04 23:51:57 -0700

    add generic wrapper

---
+++ frontend/src/components/regular/ui_schema_wrapper.vue
@@ -0,0 +1,69 @@
+<template>
+
+  <div @mouseover="mouseover"
+       @mouseleave="mouseleave"
+       v-if="show()"
+       >
+
+    <slot>
+
+    </slot>
+
+  </div>
+</template>
+
+<script lang="ts">
+
+/*
+ 
+ *
+ *  EXAMPLE USAGE:
+ *
+
+<ui_schema
+    name="my_name">
+
+   <template>
+      <v-layout>
+
+
+      </v-layout>
+    </template>
+
+</ui_schema>
+
+ */
+
+import Vue from "vue";
+
+export default Vue.extend( {
+  name: 'ui_schema',
+  props: {
+    'name': {
+        type: String,
+        default: undefined
+     }
+  },
+  data() {
+    return {
+
+    }
+  },
+  created(){
+
+  },
+  methods: {
+    show(){
+      if (this.$props.name == undefined) { return true } 
+      return this.$store.getters.get_ui_schema(this.$props.name)
+    },
+    mouseover(event) {
+      this.$store.commit('set_ui_schema_event', [this.name, event])
+    },
+    mouseleave(event) {
+      //this.$store.commit('clear_ui_schema_event')
+    }
+  }
+}
+
+) </script>
