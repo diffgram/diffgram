62627b7283d427f149516df551dafa438d7d04c6
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-10-05 18:11:36 -0700

    prevent sending events in sandbox

--- shared/database/event/event.py
+++ shared/database/event/event.py
@@ -310,6 +310,8 @@ class Event(Base):
             Sends the current event to Diffgram's EventHub for anonymous data tracking.
         :return:
         """
+        if settings.DIFFGRAM_SYSTEM_MODE in ['sandbox']:
+            return
 
         try:
             event_data = self.serialize()
