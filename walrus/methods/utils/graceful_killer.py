28ae2e993b8a0e9afbb3cd8f94fad2ebac33e09c
Pablo <pjestradac@gmail.com>
2021-10-12 09:25:58 -0600

    fix: add graceful killing mechanism to prevent stale inputs

--- walrus/methods/utils/graceful_killer.py
+++ walrus/methods/utils/graceful_killer.py
@@ -1,11 +1,14 @@
-import signal
 import time
 import sys
+import signal
 from walrus.methods.regular.regular_api import logger
 from shared.helpers.sessionMaker import session_scope
+from shared.utils.singleton import Singleton
+from signal import signal, SIGPIPE, SIG_DFL, SIGINT, SIGTERM, SIG_IGN
 
 
-class GracefulKiller:
+
+class GracefulKiller(metaclass = Singleton):
     kill_now = False
     killing_gracefully = False
     # To reason this do: SLEEP_TIME x MAX_CHECKS
@@ -15,18 +18,20 @@ class GracefulKiller:
 
     def __init__(self, processing_input_manager):
         self.processing_input_manager = processing_input_manager
-        signal.signal(signal.SIGINT, self.exit_gracefully)
-        signal.signal(signal.SIGTERM, self.exit_gracefully)
+        signal(SIGINT, self.exit_gracefully)
+        signal(SIGTERM, self.exit_gracefully)
+        signal(SIGPIPE, SIG_IGN)
         logger.info('Listening for SIGTERM events for graceful shutdown')
 
     def exit_process(self):
+        print('bye! :)')
         sys.exit(0)
 
     def set_inputs_with_error_status(self, input_list):
         with session_scope() as session:
             for input in input_list:
                 if not input.id and input.media_type == 'frame':
-                    p_media = process_media.Process_Media(input = input, session = session)
+                    p_media = Process_Media(input = input, session = session)
                     p_media.proprogate_frame_instance_update_errors_to_parent()
                 else:
                     input.status = "failed"
@@ -40,38 +45,39 @@ class GracefulKiller:
         :param args:
         :return:
         """
-        import walrus.methods.input.process_media as process_media
+        from walrus.methods.input.process_media_queue_manager import process_media_queue_manager
 
+        print('EXISTING GRACEFULLY')
         if self.killing_gracefully:
             return
 
         self.killing_gracefully = True
-        process_media.STOP_PROCESSING_DATA = True
-        num_retries = 0
+        process_media_queue_manager.STOP_PROCESSING_DATA = True
 
-        logger.warning('Staring Graceful Shutdown...')
-        while num_retries < self.MAX_CHECKS:
-            # Check if there are still inputs processing
-            if process_media.VIDEO_QUEUE.qsize() == 0 \
-                and process_media.FRAME_QUEUE.qsize() == 0 \
-                and len(self.processing_input_manager.items) == 0:
-                logger.warning('No More Inputs to Process shutting down...')
-                self.kill_now = True
-                break
-            else:
-                logger.warning('Pending Data to Process: ')
-                logger.warning('VIDEO_QUEUE: {}'.format(process_media.VIDEO_QUEUE.qsize()))
-                logger.warning('FRAME_QUEUE: {}'.format(process_media.FRAME_QUEUE.qsize()))
-                logger.warning('PROCESSING_INPUTS: {}'.format(len(self.processing_input_manager.items)))
+        time.sleep(3600)
+        self.exit_process()
 
-            time.sleep(self.SLEEP_TIME)
-
-        if self.kill_now:
-            self.exit_process()
-        else:
-            logger.error('Unable to shutdown.MAX RETRIES After {} seconds.'.format(self.MAX_CHECKS * self.SLEEP_TIME))
-            self.set_inputs_with_error_status(process_media.processing_input_manager.items)
-
-
-if __name__ == '__main__':
-    killer = GracefulKiller()
+        # num_retries = 0
+        #
+        # logger.warning('Staring Graceful Shutdown...')
+        # while num_retries < self.MAX_CHECKS:
+        #     # Check if there are still inputs processing
+        #     if process_media.VIDEO_QUEUE.qsize() == 0 \
+        #         and process_media.FRAME_QUEUE.qsize() == 0 \
+        #         and len(self.processing_input_manager.items) == 0:
+        #         logger.warning('No More Inputs to Process shutting down...')
+        #         self.kill_now = True
+        #         break
+        #     else:
+        #         logger.warning('Pending Data to Process: ')
+        #         logger.warning('VIDEO_QUEUE: {}'.format(process_media.VIDEO_QUEUE.qsize()))
+        #         logger.warning('FRAME_QUEUE: {}'.format(process_media.FRAME_QUEUE.qsize()))
+        #         logger.warning('PROCESSING_INPUTS: {}'.format(len(self.processing_input_manager.items)))
+        #
+        #     time.sleep(self.SLEEP_TIME)
+        #
+        # if self.kill_now:
+        #     self.exit_process()
+        # else:
+        #     logger.error('Unable to shutdown.MAX RETRIES After {} seconds.'.format(self.MAX_CHECKS * self.SLEEP_TIME))
+        #     self.set_inputs_with_error_status(process_media.processing_input_manager.items)
--- walrus/methods/utils/singleton_thread_safe.py
+++
@@ -1,7 +0,0 @@
-def singleton(class_):
-    instances = {}
-    def getinstance(*args, **kwargs):
-        if class_ not in instances:
-            instances[class_] = class_(*args, **kwargs)
-        return instances[class_]
-    return getinstance
