bae7f1e08bdce576086c9e80ada1ee65626ca344
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-10-20 16:17:41 -0500

    Add typescript support to the tests and add sample of typescript test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
-}
+  preset: "@vue/cli-plugin-unit-jest/presets/no-babel",
+  moduleFileExtensions: ["js", "ts", "json", "vue"],
+  transform: {
+    ".*\\.(vue)$": "vue-jest",
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  globals: {
+    "ts-jest": {
+      tsConfig: "src/tsconfig.json"
+    }
+  }
+};
