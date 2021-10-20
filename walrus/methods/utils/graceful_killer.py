d7478273e87611140a29d2c5b5e1a97252b872df
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-10-20 15:49:07 -0700

    Fix: Imports
    In deployment context methods becomes top level thing so `walrus.methods` is not found

--- walrus/methods/utils/graceful_killer.py
+++ walrus/methods/utils/graceful_killer.py
@@ -26,7 +26,7 @@ class GracefulKiller(metaclass = Singleton):
         sys.exit(0)
 
     def set_inputs_with_error_status(self, item_list):
-        from walrus.methods.input.process_media import Process_Media
+        from methods.input.process_media import Process_Media
         with session_scope() as session:
             for item in item_list:
                 if item.input is not None and item.input.id is None and item.input.media_type in ['frame']:
@@ -47,7 +47,7 @@ class GracefulKiller(metaclass = Singleton):
         :param args:
         :return:
         """
-        from walrus.methods.input.process_media_queue_manager import process_media_queue_manager
+        from methods.input.process_media_queue_manager import process_media_queue_manager
 
         logger.info('Exiting gracefully...')
         if self.killing_gracefully:
