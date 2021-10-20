238a0f7bf0aafdaa8013268656cdcc25be3a7f1c
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-10-20 11:33:38 -0500

    Add login and command tests, run jest with coverage, add coverage to gitignore

--- frontend/tests/unit/user/login.spec.js
+++ frontend/tests/unit/user/login.spec.js
@@ -22,4 +22,22 @@ describe("login.vue", () => {
     const hasForm = wrapper.find("form");
     expect(hasForm.html()).toBeTruthy();
   });
+
+  it("Displays 'Already Logged In.' message to logged in users", () => {
+    const message = "Already Logged In.";
+    const wrapper = shallowMount(login, {
+      mocks: {
+        $store: {
+          state: {
+            user: {
+              logged_in: true
+            }
+          }
+        }
+      },
+      localVue
+    });
+    const showedMessage = wrapper.text();
+    expect(showedMessage).toMatch(message);
+  });
 });
