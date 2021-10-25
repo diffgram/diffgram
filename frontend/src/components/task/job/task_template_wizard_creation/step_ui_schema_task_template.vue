2b92a3977ad83c3efbc07f9fbe3fbb948522e41c
Pablo <pjestradac@gmail.com>
2021-10-25 14:13:47 -0600

    wip: ui schema step

--- frontend/src/components/task/job/task_template_wizard_creation/step_ui_schema_task_template.vue
+++ frontend/src/components/task/job/task_template_wizard_creation/step_ui_schema_task_template.vue
@@ -2,7 +2,7 @@
   <v-container fluid>
     <div class="d-flex mb-8 justify-space-between">
       <h1 class="font-weight-medium text--primary mr-4">
-        Attach Directories
+        Select UI Schema
       </h1>
       <tooltip_button
         tooltip_message="Upload File"
@@ -16,27 +16,18 @@
 
     <v_error_multiple :error="error"></v_error_multiple>
     <p class="text--primary">
-      Select The Datasets to attach to your task template
+      UI Schemas are a way to Customize the Annotation UI. You can show/hide buttons and configure
+      what you want the annotators to see.
     </p>
 
-    <v-row class="d-flex justify-space-between">
-      <v-col cols="6">
-        <job_file_routing
-          :job="job"
-          style="border-right: 2px solid #e6e6e6"
-          :latest_dataset="latest_dataset"
-          :project_string_id="project_string_id"
-          ref="job_file_routing"
-          @directories_updated="on_attached_dirs_updated"
-          @output_dir_actions_update="on_output_dirs_updated"
-        ></job_file_routing>
+    <v-container fluid>
+      <ui_schema_selector
+        :project_string_id="project_string_id"
+        @change="on_change_schema">
 
-      </v-col>
-      <v-col cols="6">
-        <job_pipeline_mxgraph ref="job_pipepline" :job_object="job"></job_pipeline_mxgraph>
-      </v-col>
-    </v-row>
+      </ui_schema_selector>
 
+    </v-container>
 
     <v-container fluid class="mt-8 pa-0 d-flex justify-space-between" style="width: 100%">
       <v-btn x-large color="primary" @click="$emit('previous_step')">Previous</v-btn>
@@ -49,7 +40,7 @@
 <script lang="ts">
 
   import axios from 'axios';
-  import upload_wizard_sheet from '../../../input/upload_wizard_sheet'
+  import ui_schema_selector from '../../../ui_schema/ui_schema_selector'
   import job_file_routing from '../job_file_routing'
   import job_pipeline_mxgraph from '../job_pipeline_mxgraph'
 
@@ -64,14 +55,20 @@
 
       components: {
         job_file_routing,
-        upload_wizard_sheet,
         job_pipeline_mxgraph,
+        ui_schema_selector,
       },
 
       data() {
         return {
           error: {},
-          latest_dataset: undefined,
+          loading_schemas: false,
+          ui_schema_list: [
+            {
+              label: 'Default Diffgram UI',
+              value: undefined
+            }
+          ],
           output_dir: {},
           open_wizard: false,
           request_refresh_labels: new Date(),
@@ -85,42 +82,7 @@
 
       },
       methods: {
-        verify_labels: function(){
-          if(!this.$props.job.label_file_list || this.$props.job.label_file_list.length === 0){
-            this.error = {
-              name: 'At least 1 user should be assigned to the task template.'
-            }
-            return false
-          }
-          return true
-        },
-        on_next_button_click: function(){
-          this.error = {};
-          let labels_ok = this.verify_labels();
-          if(labels_ok){
-            this.$emit('next_step');
-          }
-        },
-        on_attached_dirs_updated: function(attached_dirs){
-          this.latest_dataset = attached_dirs[attached_dirs.length - 1];
-          this.job.attached_directories_dict = {
-            attached_directories_list: attached_dirs.map(elm => elm)
-          }
-        },
-        on_output_dirs_updated: async function(output_dir){
-          this.output_dir = output_dir;
-          if(!this.$refs.job_pipeline){
-            return
-          }
-          await this.$refs.job_pipepline.get_directory()
-          this.$refs.job_pipepline.redraw()
-        },
-        open_upload_wizard: async function(){
-          this.open_wizard = true;
-          await this.$nextTick();
-          this.$refs.upload_wizard_sheet.open();
-        },
-        on_close_wizard: function(){
+        on_change_schema: function(){
 
         }
       }
