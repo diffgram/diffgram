37e107b9a542000c486eaa281fcfad2a99b75cdd
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-10-25 18:51:46 -0700

    Refactor: Commonly used ones into commands

--- frontend/cypress/integration/diffgram/annotate_files/annotate_files_attributes_spec.js
+++ frontend/cypress/integration/diffgram/annotate_files/annotate_files_attributes_spec.js
@@ -16,77 +16,53 @@ describe('Annotate Files Tests', () => {
       cy.createLabels(labelsForAttributes)
       //cy.uploadAndViewSampleImage(testUser.project_string_id);
 
+      cy.goToSchemaFromToolbar()
+
     })
     const next_wizard_step = '[data-cy=wizard_navigation_next]'
     const wizard_step_container =  (step) => {return `[data-cy=attribute_wizard_step_${step}]`};
     const selectAttribute = labelsForAttributes[0].attributes.filter(attr => attr.type === 'select')[0];
 
+
     context('It Creates And Sets Value of Select Type', () => {
+
+       it('Required label for test should exist', () => {
+         cy.get(`[data-cy='${labelsForAttributes[0].name}']`).should('exist')
+       })
 
        it('Creates Select Attribute', () => {
       
-         cy.wait(3000);
-         cy.get('#open_main_menu > .v-btn__content').click({force: true});
-         cy.get('[data-cy=main_menu_labels]').click({force:true})
-         cy.wait(2000)
+         cy.createAndSelectNewAttributeGroup()
 
-         cy.get(`[data-cy='${labelsForAttributes[0].name}']`).should('exist')
-         cy.get(`[data-cy=new_attribute_button]`).click({force: true});
-         cy.get(`[data-cy="attribute_group_header_Untitled Attribute Group"]`).first().click({force: true});
          cy.get('[data-cy=attribute_kind_select]').click({force: true});
          cy.get('.v-list.v-select-list div').contains('Select').click({force: true})
     
-         cy.get(`${wizard_step_container(1)} ${next_wizard_step}`).click({force: true})
-    
-         cy.get('[data-cy=attribute_prompt]').click({force: true});
-         cy.get('[data-cy=attribute_prompt]').type(selectAttribute.prompt);
-         //cy.get('[data-cy=attribute_tag]').click({force: true});
-         //cy.get('[data-cy=attribute_tag]').type(selectAttribute.tag);
-
        })
 
-       it('Attaches Attributes to Labels', () => {
+      it('Names Select Attribute', () => {
+        cy.get(`${wizard_step_container(1)} ${next_wizard_step}`).click({force: true})
 
+        cy.typesAttributePrompt(selectAttribute.prompt)
+      })
+
+      it('Attaches Attributes to Label', () => {
          cy.get(`${wizard_step_container(2)} ${next_wizard_step}`).click({force: true})
-         cy.wait(2000)
-         cy.get('[data-cy=label_select_attribute]').click({force: true});
-         cy.wait(300)
-         cy.get('.v-menu__content .v-list.v-select-list .v-list-item span span').contains(
-           labelsForAttributes[0].name).first().click({force: true})
-
+         cy.selectLabel(labelsForAttributes[0].name)
       })
 
       it('Creates new options for Attributes', () => {
 
          cy.get(`${wizard_step_container(3)} ${next_wizard_step}`).click({force: true})
-
-         cy.get('[data-cy=new_attribute_option_button]').click({force: true});
+         cy.createAttributeOptions(selectAttribute.options)
     
-         cy.get(`${wizard_step_container(3)} ${next_wizard_step}`).click({force: true})
-         cy.wait(400);
-         for(let option of selectAttribute.options){
-           cy.get('[data-cy=attribute_option_name]').click({force: true});
-           cy.wait(750)
-           cy.get('[data-cy=attribute_option_name]').type(option, {force: true});
-           cy.get('[data-cy="create_attribute_option"] > .v-btn__content').click({force: true});
-    
-         }
-         cy.get('[data-cy="close_button_new_attribute"]').click({force: true});
-  
-    
-         cy.get(`${wizard_step_container(4)} ${next_wizard_step}`).click({force: true})
-    
-         cy.get('[data-cy=attribute_kind_select]').click({force: true});
        })
     
        it('Sets the value for the select attribute in the studio', () =>{
          cy.wait(3000);
          const selectAttribute = labelsForAttributes[0].attributes.filter(attr => attr.type === 'select')[0];
     
-         cy.get('#open_main_menu > .v-btn__content').click({force: true});
-         cy.get('[data-cy="main_menu_data_explorer"]').click({force: true});
-         cy.wait(5000);
-         cy.get('[data-cy="minimize-file-explorer-button"] > .v-btn__content').click({force: true});
+         cy.goToStudioFromToolbar()
+
          cy.select_label('car with Attributes');
     
          // Draw a box
@@ -119,39 +95,39 @@ describe('Annotate Files Tests', () => {
      })
 
     context('It Creates And Sets Value of Free Text Type', () => {
+
+      const selectAttribute = labelsForAttributes[0].attributes.filter(attr => attr.type === 'text')[0];
+
       it('Creates Free Text Attribute', () => {
-        cy.wait(3000);
-        cy.get('#open_main_menu > .v-btn__content').click({force: true});
-        cy.get('[data-cy=main_menu_labels]').click({force:true});
-        cy.wait(2000)
-        cy.get(`[data-cy='${labelsForAttributes[0].name}']`).should('exist')
-        const selectAttribute = labelsForAttributes[0].attributes.filter(attr => attr.type === 'text')[0];
-        cy.get(`[data-cy=new_attribute_button]`).click({force: true});
-        cy.get(`[data-cy="attribute_group_header_Untitled Attribute Group"]`).click({force: true});
+
+        cy.goToSchemaFromToolbar()
+
+        cy.createAndSelectNewAttributeGroup()
+
         cy.get('[data-cy=attribute_kind_select]').click({force: true});
         cy.get('.v-list.v-select-list div').contains('Free Text').click({force: true})
-        cy.get(`${wizard_step_container(1)} ${next_wizard_step}`).click({force: true})
-        cy.get('[data-cy=attribute_prompt]').click({force: true});
-        cy.get('[data-cy=attribute_prompt]').type(selectAttribute.prompt);
-        cy.get('[data-cy=attribute_tag]').click({force: true});
-        cy.get('[data-cy=attribute_tag]').type(selectAttribute.tag);
-        cy.get(`${wizard_step_container(2)} ${next_wizard_step}`).click({force: true})
-        cy.get('[data-cy=label_select_attribute]').click({force: true});
-        cy.get('.v-menu__content .v-list.v-select-list div span').contains(labelsForAttributes[0].name).first().click({force: true})
-        cy.get('[data-cy=attribute_kind_select]').click({force: true});
-        cy.get(`${wizard_step_container(3)} ${next_wizard_step}`).click({force: true})
+      })
 
+      it('Names Free Text Attribute', () => {
+        cy.get(`${wizard_step_container(1)} ${next_wizard_step}`).click({force: true})
+        cy.typesAttributePrompt(selectAttribute.prompt)
+
+      })
+
+      it('Attaches Free Text Attributes to Labels', () => {
+
+        cy.get(`${wizard_step_container(2)} ${next_wizard_step}`).click({force: true})
+        cy.selectLabel(labelsForAttributes[0].name)
+
+        cy.get(`${wizard_step_container(3)} ${next_wizard_step}`).click({force: true})
         cy.get(`${wizard_step_container(4)} ${next_wizard_step}`).click({force: true})
       })
 
       it('Sets the value for the free text attribute in the studio', () =>{
         cy.wait(3000);
         const selectAttribute = labelsForAttributes[0].attributes.filter(attr => attr.type === 'text')[0];
-        cy.get('#open_main_menu > .v-btn__content').click({force: true});
-        cy.get('[data-cy="main_menu_data_explorer"]').click({force: true});
-        cy.wait(2000);
-        cy.get('[data-cy="minimize-file-explorer-button"] > .v-btn__content').click({force: true});
-        cy.select_label('car with Attributes');
+
+        cy.goToStudioFromToolbar()
 
         // Draw a box
         cy.mousedowncanvas(75, 75);
@@ -182,6 +158,30 @@ describe('Annotate Files Tests', () => {
     })
 
     context('It Creates And Sets Value of Multiple Select Type', () => {
+
+      it('Creates Multiple Select Attribute', () => {
+
+         cy.goToSchemaFromToolbar()
+
+         cy.createAndSelectNewAttributeGroup()
+
+         cy.get('[data-cy=attribute_kind_select]').click({force: true});
+         cy.get('.v-list.v-select-list div').contains('Select').click({force: true})
+    
+       })
+
+      it('Names Select Attribute', () => {
+        cy.get(`${wizard_step_container(1)} ${next_wizard_step}`).click({force: true})
+
+        cy.typesAttributePrompt(selectAttribute.prompt)
+      })
+
+      it('Attaches Attributes to Label', () => {
+         cy.get(`${wizard_step_container(2)} ${next_wizard_step}`).click({force: true})
+         cy.selectLabel(labelsForAttributes[0].name)
+      })
+
+
       it('Creates Free Text Attribute', () => {
         cy.wait(3000);
         cy.get('#open_main_menu > .v-btn__content').click({force: true});
