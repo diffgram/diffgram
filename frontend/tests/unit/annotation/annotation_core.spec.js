7e1871dbb6bdff55f30d339f31d1cff7f98efa55
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-10-27 12:49:10 -0500

    Mock props to mount annotation_core for the unit test

--- frontend/tests/unit/annotation/annotation_core.spec.js
+++ frontend/tests/unit/annotation/annotation_core.spec.js
@@ -1,7 +1,41 @@
+import Vuex from "vuex";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import annotation_core from "@/components/annotation/annotation_core.vue";
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
 describe("Test annotation_core", () => {
+  let props;
+
+  beforeEach(() => {
+    props = {
+      mocks: {
+        $get_sequence_color: () => {},
+        $store: {
+          state: {
+            annotation_state: {},
+            user: {
+              settings: {
+                studio_box_info: {}
+              }
+            },
+            project: {
+              current: {
+                project_string_id: ""
+              }
+            }
+          },
+          getters: {
+            get_view_issue_mode: () => {}
+          }
+        }
+      }
+    };
+  });
+
   it("Test if annotation_core mounted successfully", () => {
-    console.log("This works");
+    const wrapper = shallowMount(annotation_core, props, localVue);
+    console.log(wrapper);
   });
 });
