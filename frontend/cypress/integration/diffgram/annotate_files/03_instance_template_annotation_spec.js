0b425e9977b8065a56b8510ae6bf075a0cf39f5f
Pablo <pjestradac@gmail.com>
2021-11-02 11:28:54 -0600

    fix: e2e tests

--- frontend/cypress/integration/diffgram/annotate_files/03_instance_template_annotation_spec.js
+++ frontend/cypress/integration/diffgram/annotate_files/03_instance_template_annotation_spec.js
@@ -6,18 +6,29 @@ import testLabels from "../../../fixtures/labels.json";
 describe('Instance Template Annotation', () => {
 
   context('Draws an Instance Template', () => {
-  
+
     before(function () {
       Cypress.Cookies.debug(true, {verbose: true})
       Cypress.Cookies.defaults({preserve: ['session']})
-        
+
       cy.loginByForm(testUser.email, testUser.password);
       cy.gotToProject(testUser.project_string_id);
       cy.createLabels(testLabels)
       cy.uploadAndViewSampleImage(testUser.project_string_id);
-      cy.wait(1000)
-      cy.goToStudioFromToolbar()
 
+
+    })
+
+    const template_name = 'instance template for annotation';
+    const nodes = [{x: 100, y: 100}, {x: 300, y: 300}, {x: 400, y: 400}, {x: 75, y: 150}];
+    it('Creates Instance Template For Purpose Of Annotating in Next Step', () => {
+      cy.createInstanceTemplate(template_name, {
+        nodes: nodes,
+        edges: [[0, 1], [0, 2], [0, 3]],
+      });
+      cy.get('[data-cy=save_instance_template_button]').click({force: true});
+      cy.wait(5000);
+      cy.goToStudioFromToolbar()
     })
 
     it('[In Studio] Hides ghost instance info box to ensure out of way for instance templates', () => {
@@ -32,7 +43,7 @@ describe('Instance Template Annotation', () => {
       cy.wait(2000);
       cy.get('[data-cy="instance-type-select"]').click({force: true})
       cy.wait(1000);
-      cy.get('.v-list.v-select-list div').contains('template').click({force: true})
+      cy.get('.v-list.v-select-list div').contains(template_name).click({force: true})
     })
 
     it('[In Studio] Draws a Created Instance Template', () => {
