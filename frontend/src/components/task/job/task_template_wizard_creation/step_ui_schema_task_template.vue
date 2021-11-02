fbffbc67e432d7279d6becb19d84d09f20cc3e97
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-11-01 18:50:53 -0700

    Rename to keep branding of "White-label Customization"

--- frontend/src/components/task/job/task_template_wizard_creation/step_ui_schema_task_template.vue
+++ frontend/src/components/task/job/task_template_wizard_creation/step_ui_schema_task_template.vue
@@ -2,14 +2,15 @@
   <v-container fluid data-cy="task-template-ui-schema-step">
     <div class="d-flex mb-8 justify-space-between">
       <h1 data-cy="task-template-ui-schema-step-title" class="font-weight-medium text--primary mr-4">
-        Select UI Schema:
+        White-label Customization (BETA)
       </h1>
       <tooltip_button
-        tooltip_message="Manage UI Schema"
+        tooltip_message="Launch UI Editor"
         @click="open_ui_schema_creation"
         button_color="primary"
-        icon="mdi-cog"
-        button_message=" Manage UI Schemas"
+        icon="mdi-puzzle-edit"
+        :left="true"
+        button_message="Launch UI Editor"
         color="white">
       </tooltip_button>
     </div>
@@ -17,7 +18,7 @@
     <v_error_multiple :error="error"></v_error_multiple>
     <p class="text--primary">
       UI Schemas are a way to Customize the Annotation UI. You can show/hide buttons and configure
-      what you want the annotators to see.
+      what you want the annotators to see beyond Label Schema.
     </p>
 
     <v-container fluid>
@@ -45,51 +46,47 @@
 
 <script lang="ts">
 
-  import axios from 'axios';
-  import ui_schema_selector from '../../../ui_schema/ui_schema_selector'
+import ui_schema_selector from '../../../ui_schema/ui_schema_selector'
 
-  import Vue from "vue";
+import Vue from "vue";
 
-  export default Vue.extend({
-      name: 'step_ui_schema_task_template',
-      props: [
-        'project_string_id',
-        'job',
-        'loading_steps',
-      ],
+export default Vue.extend({
+  name: 'step_ui_schema_task_template',
+  props: [
+  ],
 
-      components: {
-        ui_schema_selector,
-      },
+  components: {
+    ui_schema_selector,
+  },
 
-      data() {
-        return {
-          error: {},
-          open_wizard: false,
-        }
-      },
-      created() {
-
-      },
-
-      computed: {},
-      methods: {
-        on_change_schema: function (ui_schema) {
-          this.job.ui_schema_id = ui_schema.id;
-        },
-        on_next_button_click: function () {
-          this.$emit('next_step');
-        },
-        open_ui_schema_creation: function () {
-          let routeData = this.$router.resolve({
-            path: `/studio/annotate/${this.project_string_id}`,
-            query: {
-              edit_schema: true,
-              create_new_on_load: true
-              }
-          });
-          window.open(routeData.href, '_blank');
-        },
-      }
+  data() {
+    return {
+      error: {},
+      open_wizard: false,
     }
+  },
+  created() {
+
+  },
+
+  computed: {},
+  methods: {
+    on_change_schema: function (ui_schema) {
+      this.job.ui_schema_id = ui_schema.id;
+    },
+    on_next_button_click: function () {
+      this.$emit('next_step');
+    },
+    open_ui_schema_creation: function () {
+      let routeData = this.$router.resolve({
+        path: `/studio/annotate/${this.project_string_id}`,
+        query: {
+          edit_schema: true,
+          create_new_on_load: true
+          }
+      });
+      window.open(routeData.href, '_blank');
+    },
+  }
+}
   ) </script>
