c5709232c2c2d8542bad591d3133b749134b8ab1
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-11-02 14:30:52 -0600

    [skip ci] Return json in the route

--- default/methods/configs/mailgun_is_set.py
+++ default/methods/configs/mailgun_is_set.py
@@ -1,10 +1,15 @@
+try:
+	from methods.regular.regular_api import *
+except:
+	from default.methods.regular.regular_api import *
+
+from shared.helpers.security import limiter
 from methods import routes
-from shared.settings import settings
 
 @routes.route('/api/configs/is-mailer-set')
 def mailgun_is_set():
     is_set = settings.MAILGUN_KEY and settings.EMAIL_DOMAIN_NAME and settings != 'localhost'
     
     if is_set:
-        return "true"
-    return "false"
+        return jsonify({"mailgun": "true"})
+    return jsonify({"mailgun": "false"})
