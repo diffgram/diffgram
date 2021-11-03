04cfcb2833b90d7ee570968906c8c4adf2caf407
Vitalii Bulyzhyn <46309116+vitalii-bulyzhyn@users.noreply.github.com>
2021-11-03 13:23:13 -0600

    Add mailgun variables request (#401)
    * Add mailgun variables request
    
    * Populate .env with mailgun input
    
    * Create mailgun env variables in root repo and in fronend
    
    * Add conditional rendering
    
    * Add units for the new conditional rendering
    
    * Modify cypress login
    
    * Change variables import
    
    * Revert frontend changes
    
    * Add mailgun config route and according frontend
    
    * Add unit tests
    
    * Add extra waiting time on login e2e test
    
    * Resolve merge conflict
    
    * [skip ci] Add email domain variable to the comtainer
    
    * [skip ci] Return json in the route
    
    * Change service for new json api
    
    * fix: force typing password button for cases where mailgun api is available
    
    * fix: change tests to render conditionally based on mailgun key
    
    Co-authored-by: Pablo <pjestradac@gmail.com>

---
+++ default/methods/configs/mailgun_is_set.py
@@ -0,0 +1,15 @@
+try:
+	from methods.regular.regular_api import *
+except:
+	from default.methods.regular.regular_api import *
+
+from shared.helpers.security import limiter
+from methods import routes
+
+@routes.route('/api/configs/is-mailer-set')
+def mailgun_is_set():
+    is_set = settings.MAILGUN_KEY and settings.EMAIL_DOMAIN_NAME and settings != 'localhost'
+    
+    if is_set:
+        return jsonify({"mailgun": True})
+    return jsonify({"mailgun": False})
