7a316d28efaa2595dce44d49d8da4c5b1c2027c4
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-11-05 15:23:49 -0700

    Clean up

--- frontend/cypress/integration/diffgram/annotate_files/autoborder_spec.js
+++ frontend/cypress/integration/diffgram/annotate_files/autoborder_spec.js
@@ -1,12 +1,12 @@
 import testUser from '../../../fixtures/users.json';
 
 const points = [
-  {x: 300, y: 25},
-  {x: 400, y: 60},
-  {x: 280, y: 40},
-  {x: 260, y: 10},
-  {x: 300, y: 25},
-]
+    {x: 200, y: 25},
+    {x: 200, y: 60},
+    {x: 180, y: 40},
+    {x: 160, y: 10},
+    {x: 200, y: 25},
+    ]
 
 describe('Autoborder', () => {
 
@@ -22,7 +22,7 @@ describe('Autoborder', () => {
       cy.loginByForm(testUser.email, testUser.password, "?redirect=%2Fstudio%2Fannotate%2Fdiffgram-testing-e2e");
       cy.wait(15000)
     })
-
+    
     it('[Prep] Create a Polygon to Prepare', () => {
       cy.selectDrawValidatePolygon(points)
     })
@@ -31,12 +31,17 @@ describe('Autoborder', () => {
       cy.get('[data-cy="ok_autoborder"]').click({force: true})
     })
 
-    it('[Canvas] Selects First Autoborder Point (New Polygon)', () => {
+    it('[Canvas] New Polygon & Selects First Autoborder Point', () => {
+      cy.mousedowncanvas(points[0].x + 5, points[0].y + 5);
+      cy.mouseupcanvas()
+
       cy.mousedowncanvas(points[0].x, points[0].y);
       cy.mouseupcanvas()
     })
 
     it('[UI] Shows AutoBorder Point Selected Usage Prompt', () => {
+      cy.get('[data-cy="minimize-file-explorer-button"]').click({force: true})
+      cy.wait(300)
       cy.get('[data-cy="auto_border_first_point_selected_usage_prompt"]').should('be.visible')
     })
    
