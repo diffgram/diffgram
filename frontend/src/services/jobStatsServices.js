fbecd0f9a945b0a1b715c160b6ebeff00845741a
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-11-08 16:58:28 -0600

    Add try catch block for frontend API endpoints

--- frontend/src/services/jobStatsServices.js
+++ frontend/src/services/jobStatsServices.js
@@ -1,11 +1,27 @@
 import axios from 'axios'
 
 export const getJobStats = async (job_id) => {
-    const { data } = await axios.get(`/api/job/${job_id}/stat`)
-    return data
+    try {
+
+        const { data } = await axios.get(`/api/job/${job_id}/stat`)
+        return data
+    } catch(e) {
+        return {
+            completed: 0,
+            total: 0
+        }
+    }
 }
 
 export const getJobStatsForUser = async (job_id, user_id) => {
-    const { data } = await axios.get(`/api/job/${job_id}/user/${user_id}/stats`)
-    return data
+    try {
+
+        const { data } = await axios.get(`/api/job/${job_id}/user/${user_id}/stats`)
+        return data
+    } catch(e) {
+        return {
+            completed: 0,
+            total: 0
+        }
+    }
 }
