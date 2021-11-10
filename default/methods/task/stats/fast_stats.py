d1ff5f660d75761c7c45b01f0d7a43b349ccd3c5
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-11-10 11:10:39 -0600

    Add auth to the new routes

--- default/methods/task/stats/fast_stats.py
+++ default/methods/task/stats/fast_stats.py
@@ -5,6 +5,7 @@ from shared.database.task.task import Task
 from shared.database.annotation.instance import Instance
 
 @routes.route('/api/job/<job_id>/stat', methods=["GET"])
+@Job_permissions.by_job_id(project_role_list = ["admin", "Editor", "Viewer"], apis_user_list = ['builder_or_trainer'])
 def job_stat(job_id):
     with sessionMaker.session_scope() as session:
         job = Task.stats(
@@ -14,6 +15,7 @@ def job_stat(job_id):
         return jsonify(job)
 
 @routes.route('/api/job/<job_id>/user/<user_id>/stats', methods=["GET"])
+@Job_permissions.by_job_id(project_role_list = ["admin", "Editor", "Viewer"], apis_user_list = ['builder_or_trainer'])
 def jon_user_stats(job_id, user_id):
     with sessionMaker.session_scope() as session:
         tasks = Task.stats(
