e688dac6a90c470b62317bc349fbd1c940a24f65
vitalii-bulyzhyn <vitalybulyzhin@gmail.com>
2021-11-16 11:57:48 -0600

    [skip ci] Create dialog component

---
+++ frontend/src/components/dialogs/submit_to_review.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Submit to review
+        </v-card-title>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false"> Cancel </v-btn>
+          <v-btn color="green" text @click="dialog = false"> Submit </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "submit_to_review",
+  props: {
+    dialog: {
+      type: Boolean,
+      default: false,
+    },
+  },
+});
+</script>
