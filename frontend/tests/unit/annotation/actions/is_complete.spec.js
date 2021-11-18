63a982a3450c412bdfabc09ac25e51973b3b39da
vitalii-bulyzhyn <vitalybulyzhin@gmail.com>
2021-11-18 11:15:52 -0600

    [skip ci] Add unit test for rendering in_complete element icon

---
+++ frontend/tests/unit/annotation/actions/is_complete.spec.js
@@ -0,0 +1,39 @@
+import { shallowMount, createLocalVue } from "@vue/test-utils";
+import is_complete from "@/components/annotation/actions/is_complete.vue";
+
+describe("is_complete.vue", () => {
+    let props;
+
+    beforeEach(() => {
+        props = {
+            propsData: {
+                current_file: '',
+                task_id: 1,
+                task: {
+                    id: 1,
+                    status: ''
+                }
+            }
+        }
+    })
+
+    it ("Should render complete icon if task has status in_progress", () => {
+        const wrapper = shallowMount(is_complete, props)
+        expect(wrapper.html()).toContain('tooltip_message="Complete task"')
+        expect(wrapper.html()).toContain('icon="mdi-check-circle-outline"')
+    })
+
+    it ("Should render in review icon if task has status review_requested", () => {
+        props.propsData.task.status = 'review_requested'
+        const wrapper = shallowMount(is_complete, props)
+        expect(wrapper.html()).toContain('tooltip_message="Complete task review"')
+        expect(wrapper.html()).toContain('icon="mdi-archive-eye-outline"')
+    })
+
+    it ("Should render completed regular_chip element if task has status complete", () => {
+        props.propsData.task.status = 'complete'
+        const wrapper = shallowMount(is_complete, props)
+        expect(wrapper.findAll('regular_chip').length).toBe(1)
+        expect(wrapper.html()).toContain("Complete")
+    })
+})
