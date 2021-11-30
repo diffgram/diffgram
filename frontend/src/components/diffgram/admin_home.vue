1a234cb986bb6a90441afa0561560635321b4147
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2021-11-30 15:59:12 -0800

    Create admin_home.vue

---
+++ frontend/src/components/diffgram/admin_home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+    <project_manager :mode="'super_admin'"> </project_manager>
+
+
+  </div>
+</template>
+
+<script lang="ts">
+
+
+import axios from 'axios';
+import project_manager from '../project/project_manager'
+
+import Vue from "vue"; export default Vue.extend( {
+  name: 'admin_home',
+  components: {
+    project_manager
+  },
+  props: [''],
+  data() {
+    return {
+
+
+    }
+  },
+  mounted() {
+
+
+  },
+  methods: {
+
+  }
+}
+
+) </script>
