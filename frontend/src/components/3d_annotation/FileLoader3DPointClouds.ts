151be35a6635241cf2d62973ab4f841fa850de7c
Pablo <pjestradac@gmail.com>
2021-12-02 10:36:53 -0600

    fix: unit tests file load 3d point cloud

--- frontend/src/components/3d_annotation/FileLoader3DPointClouds.ts
+++ frontend/src/components/3d_annotation/FileLoader3DPointClouds.ts
@@ -3,19 +3,23 @@ import {PCDLoader} from 'three/examples/jsm/loaders/PCDLoader';
 import Vue from "vue/types/vue";
 
 export default class FileLoader3DPointClouds {
-  component_ctx: Vue;
+  component_ctx: {loading_pcd: boolean, percentage: number};
   pcd_loading_status: string = 'pending';
+  public pcd_loader: PCDLoader
 
-  public constructor(component_ctx) {
+  public constructor(component_ctx: {loading_pcd: boolean, percentage: number}) {
     this.component_ctx = component_ctx;
+    this.pcd_loader = new PCDLoader();
   }
 
   public async load_pcd_from_url(url) {
     return new Promise((resolve, reject) => {
-      let pcd_loader = new PCDLoader();
+      let pcd_loader = this.pcd_loader;
       pcd_loader.load(url,
         (mesh) => {
-          mesh.material.color.set('white');
+          let material = mesh.material as THREE.MeshBasicMaterial;
+
+          material.color.set('white');
           mesh.name = 'point_cloud'
           this.component_ctx.loading_pcd = false;
           // mesh.geometry.center();
