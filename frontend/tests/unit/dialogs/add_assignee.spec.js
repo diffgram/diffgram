b76a99dc6fdc37aa6e537b0d1069cf86a0334769
Vitalii Bulzyhyn <vitalybulyzhin@gmail.com>
2021-12-05 06:14:38 +0200

    Finish unit test of the assign dialog component

--- frontend/tests/unit/dialogs/add_assignee.spec.js
+++ frontend/tests/unit/dialogs/add_assignee.spec.js
@@ -2,12 +2,49 @@ import { shallowMount } from "@vue/test-utils";
 import add_assignee from "@/components/dialogs/add_assignee.vue";
 
 describe("add_assignee.vue", () => {
-  it("Does not show anything if the dialog is hidden", () => {
-    const wrapper = shallowMount(add_assignee, {
+  let props;
+
+  beforeEach(() => {
+    props = {
       propsData: {
-        dialog: false
+        dialog: true
+      },
+      mocks: {
+        $store: {
+          state: {
+            project: {
+              current: {
+                member_list: [1, 2]
+              }
+            }
+          }
+        }
       }
-    });
+    };
+  });
+
+  it("Does not show anything if the dialog is hidden", () => {
+    props.propsData.dialog = false;
+    const wrapper = shallowMount(add_assignee, props);
+
     expect(wrapper.findAll("v-dialog").length).toBe(0);
+  });
+
+  it("Show dialog when it's open", () => {
+    const wrapper = shallowMount(add_assignee, props);
+
+    expect(wrapper.findAll("v-dialog").length).toBe(1);
+  });
+
+  it("Emits assign event when submit button is pressed", () => {
+    const wrapper = shallowMount(add_assignee, props);
+    wrapper.find("#review-dialog-submit").trigger("click");
+    expect(wrapper.emitted().assign).toBeTruthy();
+  });
+
+  it("Emits close event when cancel button is pressed", () => {
+    const wrapper = shallowMount(add_assignee, props);
+    wrapper.find("#review-dialog-cancel").trigger("click");
+    expect(wrapper.emitted().close).toBeTruthy();
   });
 });
