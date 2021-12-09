27904f8dfc8a4348f793378e4364ae24302dbd84
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2021-12-08 19:14:03 -0600

    [skip ci] Add task status component

---
+++ frontend/src/components/annotation/task_status.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-cloak>
+      <v-layout>
+          <v-flex>
+              <v-card>
+                <h3>Task status:</h3>
+                <v-stepper
+                    v-model="e6"
+                    vertical
+                    >
+                    <v-stepper-step
+                        :complete="e6 > 1"
+                        step="1"
+                        >
+                            Pending
+                    </v-stepper-step>
+                    <v-stepper-step
+                        :complete="e6 > 1"
+                        step="2"
+                    >
+                        On review
+                    </v-stepper-step>
+                    <v-stepper-step
+                        :complete="e6 > 1"
+                        step="3"
+                    >
+                        Completed
+                    </v-stepper-step>
+                </v-stepper>
+              </v-card>
+          </v-flex>
+      </v-layout>
+  </div>
+</template>
+
+<script lang="ts">
+
+import Vue from "vue";
+
+  export default Vue.extend( {
+    name: 'task_status',
+}) 
+</script>
