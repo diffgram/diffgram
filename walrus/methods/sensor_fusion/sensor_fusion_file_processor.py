d28f51b3164997d5aa07bb4326af5c77fb356a21
Pablo <pjestradac@gmail.com>
2021-12-10 00:26:08 -0600

    wip latin1 codec

--- walrus/methods/sensor_fusion/sensor_fusion_file_processor.py
+++ walrus/methods/sensor_fusion/sensor_fusion_file_processor.py
@@ -32,7 +32,7 @@ class SensorFusionFileProcessor:
         self.input.status = "processing"
         self.try_to_commit()
 
-        with open(self.input.temp_dir_path_and_filename) as json_data:
+        with open(self.input.temp_dir_path_and_filename, encoding = 'latin1') as json_data:
             sensor_fusion_spec = json.load(json_data)
 
             return sensor_fusion_spec
