3bd4230c6d4adad0e22e092aa7dca121b926c25d
Pablo <pjestradac@gmail.com>
2021-12-10 00:14:48 -0600

    wip load

--- walrus/methods/sensor_fusion/sensor_fusion_file_processor.py
+++ walrus/methods/sensor_fusion/sensor_fusion_file_processor.py
@@ -32,9 +32,8 @@ class SensorFusionFileProcessor:
         self.input.status = "processing"
         self.try_to_commit()
 
-        with open(self.input.temp_dir_path_and_filename, encoding = 'utf-8', errors = 'ignore') as json_data:
-            str_data = json_data.read()
-            sensor_fusion_spec = json.loads(str_data)
+        with open(self.input.temp_dir_path_and_filename) as json_data:
+            sensor_fusion_spec = json.load(json_data)
 
             return sensor_fusion_spec
 
