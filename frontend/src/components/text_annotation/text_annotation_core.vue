4a44e4b55bbee93989bda2f65f41ea71688fdd3a
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2021-12-16 17:08:49 -0600

    [skip ci] Add annotations to the text on selection

--- frontend/src/components/text_annotation/text_annotation_core.vue
+++ frontend/src/components/text_annotation/text_annotation_core.vue
@@ -1,10 +1,6 @@
 <template>
+<div>
     <svg data-v-3863495a="" data-v-4e93770c="" version="1.1" xmlns="http://www.w3.org/2000/svg" direction="ltr" id="svg0:60" width="100%" style="height: 125.5px">
-    <defs data-v-3863495a="">
-        <marker data-v-3863495a="" id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="6" markerHeight="6" orient="auto-start-reverse">
-            <path data-v-3863495a="" d="M 0 0 L 10 5 L 0 10 z" stroke="#74b8dc" fill="#74b8dc"></path>
-        </marker>
-        </defs>
         <g data-v-3863495a="" transform="translate(0, 23.5)">
             <g data-v-3863495a="" style="cursor: pointer;">
                 <path d="M 168.125 30
@@ -18,7 +14,7 @@
                     <text x="378.0625" y="5" fill="currentColor" text-anchor="middle">isLorem</text>
                     </g></g>
                     <g data-v-3863495a="" transform="translate(0, 50)">
-                        <text data-v-5225550e="" data-v-3863495a="" fill="currentColor" id="0:60" x="0" style="white-space: pre;">After bowling Somerset out for 83 on the opening morning at </text>
+                        <text data-v-5225550e="" data-v-3863495a="" fill="currentColor" id="0:60" x="0" style="white-space: pre;">{{ text }}</text>
                         <g data-v-3863495a=""><line x1="127.4609375" y1="5" x2="208.7890625" y2="5" stroke="#e6d176" stroke-width="5" stroke-linecap="round">
                             </line><g style="cursor: pointer;"><circle r="3" fill="#e6d176" cx="127.9609375" cy="20"></circle>
                             <text x="124.9609375" y="20" fill="currentColor" dx="8" dy="0.35em">ORG</text>
@@ -27,21 +23,63 @@
                 </g>
         </g>
     </svg>
+    <br />
+    <br />
+    <br />
+    <div>
+        <svg @mousedown="on_selection_start" @mouseup="on_selection_end" id="trial">
+            <g transform="translate(0, 23.5)">
+                <text id="text-to-annotate">{{text}}</text>
+            </g>
+            <rect v-if="set_x && set_y" :x="set_x" :y="set_y" :width="selecction_width" height="3" fill="green"></rect>
+        </svg>
+    </div>
+</div>
 </template>
 
 <script>
 import Vue from "vue";
-import axios from "axios"
 
 export default Vue.extend({
     name: "text_annotation_core",
     props: {
         file: {}
     },
-    async mounted() {
-        console.log("Fetch from: ", this.file.text.url_signed)
-        const get_file = await axios.get("https://diffgram-storage-v.s3.amazonaws.com/projects/text/1/1")
-        console.log(get_file)
+    data() {
+        return {
+            text: "This is my amazing text",
+            set_x: null,
+            set_y: null,
+            selecction_width: 70
+        }
+    },
+    methods: {
+        on_svg_click: function(event) {
+            console.log()
+            const coordX_global = event.clientX;
+            var element = document.getElementById('trial');
+            var text_element = document.getElementById('text-to-annotate');
+            var topPos = text_element.getBoundingClientRect().top + window.scrollY;
+            var leftPos = element.getBoundingClientRect().left + window.scrollX;
+
+            const coordX_local = coordX_global - leftPos
+            const coordY_local = text_element.getBoundingClientRect().bottom - topPos + 10
+
+            this.set_x = coordX_local
+            this.set_y = coordY_local
+        },
+        on_selection_start: function(event) {
+            console.log("Key down")
+            this.selecction_width = 0
+            this.on_svg_click(event)
+        },
+        on_selection_end: function(event) {
+            console.log("key up")
+            const coordX_global = event.clientX;
+            this.selecction_width = Math.abs(this.set_x - coordX_global)
+
+            console.log(this.selecction_width)
+        }
     }
 })
 </script>
