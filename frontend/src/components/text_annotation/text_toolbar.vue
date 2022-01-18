9aa16d8b2509867c21d2cb6eb17144e604a0887a
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-01-18 07:05:53 -0600

    [skip ci] Add toolbar component

---
+++ frontend/src/components/text_annotation/text_toolbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-toolbar
+        dense
+        width="100%"
+        elevation="0"
+        fixed
+        :height="height"
+        style="overflow: hidden; padding: 0; border-bottom: 1px solid #e0e0e0"
+    >
+        <v-toolbar-items>
+            <h1>Text Toolbar</h1>
+        </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+import Vue from 'vue'
+export default Vue.extend({
+    name: "text_toolbar"
+})
+</script>
