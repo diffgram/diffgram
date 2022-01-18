1a7f48b8f283d4a663d6caf57f8f53e18ad8beb7
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-01-18 06:56:34 -0600

    [skip ci] Create text Insance calss

--- frontend/src/components/vue_canvas/instances/TextInstance.ts
+++ frontend/src/components/vue_canvas/instances/TextInstance.ts
@@ -1,5 +1,57 @@
 import { Instance } from "./Instance";
+import { v4 as uuidv4 } from 'uuid'
 
-export class TextInstance extends Instance {
+export class TextAnnotationInstance extends Instance {
+    public start_token: string = null;
+    public end_token: string = null;
 
+    constructor(start_token, end_token, label_file) {
+        super();
+        this.type = "text_annotation";
+        this.creation_ref_id = uuidv4();
+        this.start_token = start_token;
+        this.end_token = end_token;
+        this.label_file = label_file;
+        this.label_file_id = label_file.id;
+    }
+
+    public update_instance_start_token(start_token): void {
+        this.start_token = start_token;
+    }
+
+    public update_instance_end_token(end_token): void {
+        this.end_token = end_token;
+    }
+
+    public get_instance_data(): object {
+        return {
+            type: this.type,
+            selected: this.selected,
+            start_token: this.start_token,
+            end_token: this.end_token,
+            label_file: this.label_file,
+            label_file_id: this.label_file_id,
+            soft_delete: this.soft_delete
+        }
+    }
+}
+
+export class TexrRelationInstance extends Instance {
+    public from_instance_id: number = null;
+    public to_instance_id: number = null;
+
+    constructor() {
+        super();
+        this.creation_ref_id = uuidv4();
+        this.type = "text_relation";
+    }
+
+    public get_instance_data(): object {
+        return {
+            type: this.type,
+            selected: this.selected,
+            from_instance_id: this.from_instance_id,
+            to_instance_id: this.to_instance_id
+        }
+    }
 }
