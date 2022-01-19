0609e326fcd04d803dea3fbad9153ae892c15ec7
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-01-19 10:34:31 -0600

    [skip ci] Add small formating to the code

--- frontend/src/components/text_annotation/text_toolbar.vue
+++ frontend/src/components/text_annotation/text_toolbar.vue
@@ -36,16 +36,14 @@
             <div style="width: 310px">
                 <div class="pl-2 pr-3 pt-4">
                     <label_select_annotation
-                    :project_string_id="project_string_id"
-                    :label_file_list="label_list"
-                    :label_file_colour_map="label_file_colour_map"
-                    @change="$emit('change_label_file', $event)"
-                    :loading="loading"
-                    :request_refresh_from_project="true"
-                    :show_visibility_toggle="true"
-                    @update_label_file_visible="
-                        $emit('update_label_file_visibility', $event)
-                    "
+                        :project_string_id="project_string_id"
+                        :label_file_list="label_list"
+                        :label_file_colour_map="label_file_colour_map"
+                        :loading="loading"
+                        :request_refresh_from_project="true"
+                        :show_visibility_toggle="true"
+                        @change="$emit('change_label_file', $event)"
+                        @update_label_file_visible="$emit('update_label_file_visibility', $event)"
                     />
                 </div>
             </div>
