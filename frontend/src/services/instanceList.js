ae009ce8e8cf3ab4bc1394b49cb10403affb063e
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-01-24 05:11:45 -0600

    [skip ci] Add get insatcnes service

--- frontend/src/services/instanceList.js
+++ frontend/src/services/instanceList.js
@@ -8,7 +8,15 @@ export const postInstanceList = async (project_string_id, file_id, instance_list
         )
         return data
     } catch(e) {
-        console.log(e)
+        return null
+    }
+}
+
+export const getInstanceList = async (project_string_id, file_id) => {
+    try {
+        const { data } = await axios.post(`/api/project/${project_string_id}/file/${file_id}/annotation/list`, {})
+        return data
+    } catch (e) {
         return null
     }
 }
