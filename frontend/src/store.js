699b7563b60ad48a7cd14dd7e92171605f222267
Pablo <pjestradac@gmail.com>
2022-02-01 12:00:11 -0600

    wip: cleanup

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -801,8 +801,6 @@ const my_store = new Vuex.Store({
   plugins: [createPersistedState({
 
     reducer: (state) => {
-      console.log('STRINGFYIONG STATE........')
-      console.log(JSON.stringify(state))
       let reducer = Object.assign({}, state)
       for(const key of Object.keys(reducer)){
         if(modulesToOmit.includes(key)){
