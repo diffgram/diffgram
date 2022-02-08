1ab77b59ea26c8f4ccd79aa3c5276dd9b23912df
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-02-07 19:13:25 -0500

    [skip ci] Predict app from crushin if there are some probelms with relation

--- frontend/src/services/instanceList.js
+++ frontend/src/services/instanceList.js
@@ -14,8 +14,12 @@ export const postInstanceList = async (project_string_id, file_id, instance_list
 
 export const getInstanceList = async (project_string_id, file_id) => {
     try {
-        const { data } = await axios.post(`/api/project/${project_string_id}/file/${file_id}/annotation/list`, {})
-        return data
+        const { data: { file_serialized: { instance_list } } } = await axios.post(`/api/project/${project_string_id}/file/${file_id}/annotation/list`, {})
+        const new_list = instance_list.filter(instance => {
+            if (instance.type !== "relation") return true
+            if (instance.from_instance_id && instance.to_instance_id) return true
+        })
+        return new_list
     } catch (e) {
         return null
     }
