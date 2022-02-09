a9b4b2df440110cd39a6bd45659a39971fccc5ea
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-02-09 11:51:22 -0500

    [skip ci] Wire up get and post instances functions of the text interface to the task context

--- frontend/src/services/instanceList.js
+++ frontend/src/services/instanceList.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
-export const postInstanceList = async (project_string_id, file_id, instance_list) => {
+export const postInstanceList = async (url, instance_list) => {
     try {
         const { data } = await axios.post(
-            `/api/project/${project_string_id}/file/${file_id}/annotation/update`, 
-            { instance_list }
+            url, 
+            { 
+                instance_list,
+                and_complete: false    
+            }
         )
         return data
     } catch(e) {
@@ -12,9 +15,9 @@ export const postInstanceList = async (project_string_id, file_id, instance_list
     }
 }
 
-export const getInstanceList = async (project_string_id, file_id) => {
+export const getInstanceList = async (url, payload = {}) => {
     try {
-        const { data: { file_serialized: { instance_list } } } = await axios.post(`/api/project/${project_string_id}/file/${file_id}/annotation/list`, {})
+        const { data: { file_serialized: { instance_list } } } = await axios.post(url, payload)
         const new_list = instance_list.filter(instance => {
             if (instance.type !== "relation") return true
             if (instance.from_instance_id && instance.to_instance_id) return true
