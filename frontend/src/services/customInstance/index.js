b3800f382bd5016ae62a7831835d5b431f1c6d1f
PJEstrada <pjestrada006@hotmail.com>
2022-02-21 12:37:44 -0600

    DAD-81 Exam Now Duplicate Files (#636)
    * fix: change status code for interceptor
    
    * fix: exams now duplicate file as expected
    
    * fix: e2e tests
    
    * fix: task_template_name flaky tests
    
    Co-authored-by: Pablo <pjestradac@gmail.com>

--- frontend/src/services/customInstance/index.js
+++ frontend/src/services/customInstance/index.js
@@ -7,7 +7,7 @@ instance.interceptors.response.use(function (response) {
     return response;
   }, async function (error) {
       const { status } = error.response
-    if (status === 403) {
+    if (status === 401) {
         await logout()
     }
     return Promise.reject(error);
