b7f962fefb23c732db26db25464068a220c8a9c6
Pablo <pjestradac@gmail.com>
2022-02-22 15:24:52 -0600

    fix: validate URL Base ends with "/"
    Now we check on startup that URL BASE end with a / to avoid misconstructed links

--- default/methods/startup/system_startup_checker.py
+++ default/methods/startup/system_startup_checker.py
@@ -9,8 +9,15 @@ class DefaultServiceSystemStartupChecker(SystemStartupBase):
     def __init__(self):
         self.service_name = settings.DIFFGRAM_SERVICE_NAME
 
+    def check_settings_values_validity(self):
+        if not settings.URL_BASE:
+            raise Exception(f'URL_BASE is not set. Please provide a value for URL_BASE')
+        if not settings.URL_BASE.endswith('/'):
+            raise Exception(f'Invalid URL_BASE value setting: "{settings.URL_BASE}". URL_BASE should end with "/"')
+
     def execute_startup_checks(self):
         logger.info('[{}] Performing System Checks...'.format(self.service_name))
+        self.check_settings_values_validity()
         result = SystemEvents.system_startup_events_check(self.service_name)
         if not result:
             raise Exception('System Startup Check Failed.')
