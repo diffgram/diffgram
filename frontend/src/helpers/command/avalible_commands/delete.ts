022f221bfd59065084d6dbbf3f6bb69c7d0ccc55
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-03-08 11:45:48 -0500

    [skip ci] Fix bug on the new delete command

--- frontend/src/helpers/command/avalible_commands/delete.ts
+++ frontend/src/helpers/command/avalible_commands/delete.ts
@@ -4,20 +4,24 @@ import { Command } from "../command"
 export default class DeleteInstanceCommand extends Command {
     private deleted_relations: Array<number> = [];
 
-    private find_relations(instance: Instance) {
+    private find_linked_relations(instance: Instance) {
         const { id } = instance.get_instance_data()
         
         this.instance_list.get_all()
             .map((inst, index) => {
                 const { from_instance_id, to_instance_id } = inst.get_instance_data()
-                if (inst.type === "relation" && (id === from_instance_id || id === to_instance_id)) this.deleted_relations.push(index)
+                if (
+                    inst.type === "relation" && 
+                    (id === from_instance_id || id === to_instance_id)
+                    && !inst.soft_delete
+                ) this.deleted_relations.push(index)
             })
     }
 
     execute() {        
         this.instances.map((instance, index) => {
             instance.soft_delete = true;
-            this.find_relations(instance)
+            this.find_linked_relations(instance)
             this.instance_list.replace(instance, this.replacement_indexes[index])
         })
     }
