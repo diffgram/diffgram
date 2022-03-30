841b3bcbf84f6de052c9f03a99587492da095ba7
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-03-30 11:56:03 -0500

    Determine instances for the Geo V1

--- frontend/src/components/vue_canvas/instances/GeoInstance.ts
+++ frontend/src/components/vue_canvas/instances/GeoInstance.ts
@@ -2,3 +2,13 @@ import { Instance } from "./Instance";
 import { v4 as uuidv4 } from 'uuid'
 
 class GeoCircle extends Instance {}
+
+class GeoBox extends Instance {}
+
+class GeoLine extends Instance {}
+
+class GeoPoint extends Instance {}
+
+class GeoTag extends Instance {}
+
+class GeoPoligon extends Instance {}
