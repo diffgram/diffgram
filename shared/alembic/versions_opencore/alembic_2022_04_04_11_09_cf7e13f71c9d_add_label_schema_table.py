206bff610efdceeef34535c4324850b62c1bc12b
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-04-06 00:53:28 -0700

    Create index on attribute_template and project_id
    Suggesting we role this into one almebic thing so it doesn't conflict
    Context of improving performance with 1000+ attributes

--- shared/alembic/versions_opencore/alembic_2022_04_04_11_09_cf7e13f71c9d_add_label_schema_table.py
+++ shared/alembic/versions_opencore/alembic_2022_04_04_11_09_cf7e13f71c9d_add_label_schema_table.py
@@ -42,12 +42,15 @@ def upgrade():
                     )
     op.create_index('index_label_schema_link_schema_id', 'label_schema_link', ['schema_id'])
     op.create_index('index_label_schema_link_label_file_id', 'label_schema_link', ['label_file_id'])
+    
+    op.create_index('index_attribute_template_project_id', 'attribute_template', 'project_id')
 
 
 def downgrade():
     op.drop_index('index_label_schema_project_id', 'label_schema')
     op.drop_index('index_label_schema_link_schema_id', 'label_schema_link')
     op.drop_index('index_label_schema_link_label_file_id', 'label_schema_link')
+    op.drop_index('index_attribute_template_project_id', 'attribute_template')
 
     op.drop_table('label_schema')
     op.drop_table('label_schema_link')
