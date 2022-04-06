ef3fd5667c21511cff33a37f318d94482f60f29e
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-04-06 01:13:51 -0700

    Removed deprecated old code
    -> Color was moved directly to file
    -> Those old automatic relationship / list back populating was slow and sub optimal as far as last checked

--- shared/database/label.py
+++ shared/database/label.py
@@ -8,23 +8,11 @@ class Label(Base):
     id = Column(Integer, primary_key = True)
     name = Column(String(200))
 
-    # TODO colour is deprecated given storing in file directly now?
-    # ie a name may be "universal" but the colour is not?
-
-    # example of colour {"hex": "#194d33", "hsl": {"l": 0.2, "h": 150, "s": 0.5, "a": 1},
-    # "rgba": {"a": 1, "g": 77, "r": 25, "b": 51}, "hsv": {"a": 1, "h": 150, "s": 0.66, "v": 0.3}, "a": 1}
-    colour = Column(MutableDict.as_mutable(JSONEncodedDict))
-
-    # Remove soft delete?
     soft_delete = Column(Boolean)
 
     # project_id = Column(Integer, ForeignKey('project.id'))
     # project = relationship("Project", back_populates="label_list")
 
-    # TODO review on context of source control
-    # not sure if this makes sense anymore? And may be part of slowing down thing...
-    # instance_list = relationship("Instance", back_populates="label")
-    # sequence_list = relationship("Sequence", back_populates="label")
 
     @staticmethod
     def get_by_name(session, label_name):
