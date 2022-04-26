a8eb2091bb9dcb326f7df5fef7fc775cfbc24ee0
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-04-25 17:57:51 -0700

    more

--- frontend/src/components/action/workflow_steps_visualizer.vue
+++ frontend/src/components/action/workflow_steps_visualizer.vue
@@ -92,6 +92,18 @@
               },
             },
             {
+              is_trigger: true,
+              icon: 'mdi-folder-arrow-up',
+              kind: 'file_upload',
+              preview: {
+                title: 'Receive/Wait for Start from External Process',
+              },
+              node: {
+                title: 'Receive/Wait forStart from External Process',
+                description: '...',
+              },
+            },
+            {
               is_trigger: false,
               icon: 'mdi-folder-arrow-up',
               kind: 'file_upload',
@@ -204,6 +216,66 @@
               icon: 'mdi-filter',
               kind: 'filter',
               preview: {
+                title: 'Train Azure Model',
+              },
+              node: {
+                title: 'Train Azure Model',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Validate Model Performance',
+              },
+              node: {
+                title: 'Validate Model Performance',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Create an Issue If',
+              },
+              node: {
+                title: 'Create an Issue If',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Wait until all issues are resolved',
+              },
+              node: {
+                title: 'Wait until all issues are resolved',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Condition on a class',
+              },
+              node: {
+                title: 'Condition on a class',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
                 title: 'Alert / Email',
               },
               node: {
@@ -240,10 +312,10 @@
               icon: 'mdi-filter',
               kind: 'filter',
               preview: {
-                title: 'Loop Back to a Step',
+                title: 'Deploy Dataset',
               },
               node: {
-                title: 'Loop Back to a Step',
+                title: 'Deploy Dataset',
                 description: '...',
               },
             },
@@ -252,10 +324,94 @@
               icon: 'mdi-filter',
               kind: 'filter',
               preview: {
-                title: 'Human Supervision Control Point',
+                title: 'Query Data',
               },
               node: {
-                title: 'Human Supervision Control Point',
+                title: 'Query Data',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Visualize Data',
+              },
+              node: {
+                title: 'Visualize Data',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Visualize Human Performance',
+              },
+              node: {
+                title: 'Visualize Human Performance',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Show Customized (UI Schema) Studio View',
+              },
+              node: {
+                title: 'Show Customized (UI Schema) Studio View',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Show Customized (UI Schema) Data Explorer View',
+              },
+              node: {
+                title: 'Show Customized (UI Schema) Data Explorer View',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Visualize Model Performance',
+              },
+              node: {
+                title: 'Visualize Model Performance',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Move/Copy Data',
+              },
+              node: {
+                title: 'Move/Copy Data',
+                description: '...',
+              },
+            },
+            {
+              is_trigger: false,
+              icon: 'mdi-filter',
+              kind: 'filter',
+              preview: {
+                title: 'Stop Until / Human Supervision Control Point',
+              },
+              node: {
+                title: 'Stop Until / Human Supervision Control Point',
                 description: '...',
               },
             },
