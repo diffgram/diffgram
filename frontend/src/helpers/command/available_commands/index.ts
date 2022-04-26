8a11771c20545252a91bcf16eba24c198d51629d
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-04-26 11:11:07 -0500

    Add undo/redo command for instance translation

--- frontend/src/helpers/command/available_commands/index.ts
+++ frontend/src/helpers/command/available_commands/index.ts
@@ -2,10 +2,12 @@ import CreateInstanceCommand from "./create"
 import UpdateInstanceCommand from "./update"
 import DeleteInstanceCommand from "./delete"
 import UpdateInstanceLabelCommand from "./update_label"
+import UpdateInstanceGeoPositionCommand from "./update_geo_position"
 
 export {
     CreateInstanceCommand,
     UpdateInstanceCommand,
     DeleteInstanceCommand,
-    UpdateInstanceLabelCommand
+    UpdateInstanceLabelCommand,
+    UpdateInstanceGeoPositionCommand
 };
---
+++ frontend/src/helpers/command/available_commands/update_geo_position.ts
@@ -0,0 +1,45 @@
+import { InstanceInterface } from "../../interfaces/InstanceData";
+import { Command } from "../command";
+import { transform } from 'ol/proj';
+
+export default class UpdateInstanceGeoPositionCommand extends Command {
+    private bounds: Array<Array<number>>;
+    private lonlat_bounds: Array<Array<number>>;
+    private initial_instances: Array<InstanceInterface> = [];
+
+    public set_new_geo_coords(bounds: Array<Array<number>>): void {
+        this.lonlat_bounds = bounds.map(bound => transform(bound, 'EPSG:3857', 'EPSG:4326'))
+        this.bounds = [...bounds];
+    }
+
+    execute() {
+        if (this.initial_instances.length === 0) this.initial_instances = this.instances.map(inst => this._copyInstance(inst))
+
+        this.instances.forEach((instance, index, instanceArray) => {
+            if (instance.type === 'geo_circle' || instance.type === "geo_point") {
+                instanceArray[index]["coords"] = this.bounds[0]
+                instanceArray[index]["lonlat"] = this.lonlat_bounds[0]
+            } else {
+                instanceArray[index]["bounds"] = this.bounds
+                instanceArray[index]["bounds_lonlat"] = this.lonlat_bounds
+            }
+        })
+
+        this.instances.map((instance, index) => {
+            this.instance_list.replace(instance, this.replacement_indexes[index])
+        })
+    }
+
+    undo() {
+        this.initial_instances.map((instance, index) => {
+            const instance_to_modify = this.instance_list.get_all()[this.replacement_indexes[index]]
+            if (instance.type === 'geo_circle' || instance.type === "geo_point") {
+                instance_to_modify["coords"] = [...instance["coords"]]
+                instance_to_modify["lonlat"] = [...instance["lonlat"]]
+            } else {
+                instance_to_modify["bounds"] = [...instance["bounds"]]
+                instance_to_modify["bounds_lonlat"] = [...instance["bounds_lonlat"]]
+            }
+        })
+    }
+}
