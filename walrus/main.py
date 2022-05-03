8504ba38da911527bd82f3285ac7d7cd3f7fe100
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-05-03 16:35:01 -0700

    Remove datasaur - own text interface now
    the existing datasaur code can be shifted to a migration context, eg. easy migrate from datasaur to diffgram, or other uses in future.
    this minimal change helps remove resources used by this
    this was already removed from task creation context before

--- walrus/main.py
+++ walrus/main.py
@@ -114,9 +114,6 @@ job_launcher_thread = TaskTemplateLauncherThread(
 sync_actions_thread = SyncActionsHandlerThread(thread_sleep_time_min=settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MIN,
                                                thread_sleep_time_max=settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MAX,
                                                run_once = False)
-datasaur_sync_manager = DatasaurSyncManager(thread_sleep_time_min=settings.DATASAUR_SYNC_THREAD_SLEEP_TIME_MIN,
-                                            thread_sleep_time_max=settings.DATASAUR_SYNC_THREAD_SLEEP_TIME_MAX)
-datasaur_sync_manager.start_sync_loop()
 
 actions_thread = ActionFlowTriggerQueueThread(thread_sleep_time_min=settings.ACTION_THREAD_SLEEP_TIME_MIN,
                                               thread_sleep_time_max=settings.ACTION_THREAD_SLEEP_TIME_MAX,
