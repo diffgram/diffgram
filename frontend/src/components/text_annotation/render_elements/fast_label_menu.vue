cfd220853a08dcf25a1210ec7883559b6756879b
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-10 14:33:51 -0500

    Add label search to the popup

--- frontend/src/components/text_annotation/render_elements/fast_label_menu.vue
+++ frontend/src/components/text_annotation/render_elements/fast_label_menu.vue
@@ -12,11 +12,12 @@
                 <v-text-field
                     label="Start by typing label name"
                     hide-details="auto"
+                    @input="on_search_label"
                 />
             </div>
             <v-list dense>
                 <v-list-item-group>
-                    <v-list-item v-for="(label, index) in label_list" :key="`labl+list_item${index}`">
+                    <v-list-item v-for="(label, index) in search_label" :key="`labl+list_item${index}`">
                         <v-list-item-content>
                             <div class="list-item" :style="`color: ${label.colour.hex}`">
                                 {{ label.label.name }} 
@@ -58,8 +59,19 @@ export default Vue.extend({
         }
     },
     data() {
-        return {}
+        return {
+            search_label: null
+        }
     },
+    mounted() {
+        this.search_label = [...this.label_list]
+    },
+    methods: {
+        on_search_label: function(e) {
+            const to_search = e.toLowerCase()
+            this.search_label = [...this.label_list].filter(label => label.label.name.toLowerCase().includes(to_search))
+        }
+    }
 })
 </script>
 
