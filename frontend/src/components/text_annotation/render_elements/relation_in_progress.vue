c74ec5f43fcb77c5c439e42eeb04cd38d3899353
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-12 11:38:40 -0500

    Extract drawing relation to a separate component

---
+++ frontend/src/components/text_annotation/render_elements/relation_in_progress.vue
@@ -0,0 +1,46 @@
+<template>
+    <g v-if="render_drawing_arrow">
+        <circle
+            :cx="render_drawing_arrow.marker.x"
+            :cy="render_drawing_arrow.marker.y"
+            :fill="solid_fill"
+            r="3"
+            class="unselectable"
+        />
+        <path
+            v-if="render_drawing_arrow.path"
+            :stroke="solid_fill"
+            :d="render_drawing_arrow.path"
+            fill="transparent"
+            class="unselectable"
+        />
+        <path
+            v-if="render_drawing_arrow.arrow"
+            :d="`M ${render_drawing_arrow.arrow.x} ${render_drawing_arrow.arrow.y} l -5, -5 l 10, 0 l -5, 5`"
+            :fill="solid_fill"
+            class="unselectable"
+        />
+    </g>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: "relation_in_progress",
+    props: {
+        render_drawing_arrow: {
+            type: Object,
+            required: true
+        }
+    },
+    data() {
+        return {
+            solid_fill: "rgba(76, 139, 245)",
+            end_border_moved: null,
+            start_border_moved: null
+        }
+    },
+    methods: {}
+})
+</script>
