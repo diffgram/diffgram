e56071dfdc1ed0006e0b0a70e846161ea64d23d1
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-05-14 19:15:08 -0700

    Add generic for regen determination
    Context that each file type usually has slightly different specific conventions, but we can still generalize the bulk of it in one place.
    
    This is still very brittle in some ways, but it solves the immediate need of being able to use the rest across all types
    
    And when we build out more on the IAM per file side, this will be a launch point potentially to generating this URL. And/or an alternative instead of caching for convenience for example)

--- shared/data_tools_core.py
+++ shared/data_tools_core.py
@@ -4,6 +4,7 @@ from shared.data_tools_core_gcp import DataToolsGCP
 from shared.data_tools_core_s3 import DataToolsS3
 from shared.data_tools_core_azure import DataToolsAzure
 from shared.utils.singleton import Singleton
+import time
 
 
 class Data_tools(metaclass = Singleton):
@@ -28,4 +29,29 @@ class Data_tools(metaclass = Singleton):
             self.data_tools = DataToolsAzure()
 
 
-data_tools = Data_tools().data_tools
+    def determine_if_should_regenerate_url(self, object, session, url_signed_expiry=None):
+        if not session: return False, None
+        minimum_days_valid = 30 * 12  # this should always be lower then new offset
+        new_offset_days_valid = 30 * 14
+        one_day = 86400
+        time_to_check = time.time() + (one_day * minimum_days_valid)
+        new_offset_in_seconds = one_day * new_offset_days_valid
+
+        if url_signed_expiry is None:
+            url_signed_expiry = object.url_signed_expiry
+
+        if url_signed_expiry is None:
+            return True, new_offset_in_seconds
+
+        if url_signed_expiry <= settings.URL_SIGNED_REFRESH + (one_day * minimum_days_valid):
+            return True, new_offset_in_seconds
+
+        if url_signed_expiry <= time_to_check:
+            return True, new_offset_in_seconds
+
+        return False, None
+
+
+data_tools_instance = Data_tools()
+data_tools = data_tools_instance.data_tools
+data_tools.determine_if_should_regenerate_url = data_tools_instance.determine_if_should_regenerate_url
