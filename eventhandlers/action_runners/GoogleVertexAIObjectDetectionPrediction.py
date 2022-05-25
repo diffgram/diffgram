211b4e48abc84f06af36a08681e2496673986a27
Pablo <pjestradac@gmail.com>
2022-05-25 14:32:28 -0600

    fix: comment to allow service startup
    for now code still wip

--- eventhandlers/action_runners/GoogleVertexAIObjectDetectionPrediction.py
+++ eventhandlers/action_runners/GoogleVertexAIObjectDetectionPrediction.py
@@ -1,10 +1,8 @@
 from eventhandlers.action_runners.ActionRunner import ActionRunner
 from shared.shared_logger import get_shared_logger
-from shared.database.task.job.job import Job
-from shared.helpers.sessionMaker import session_scope
-from shared.utils import job_dir_sync_utils
+from shared.database.action.action_template import Action_Template
 from shared.database.source_control.file import File
-from methods.input.packet import enqueue_packet
+# from methods.input.packet import enqueue_packet
 
 logger = get_shared_logger()
 
@@ -22,60 +20,63 @@ class GoogleVertexAIObjectDetectionPrediction(ActionRunner):
                    Creates a task from the given file_id in the given task template ID.
                :return:
                """
-        file_id = self.event_data['file_id']
-        if not file_id:
-            logger.warning(f'Action has no file_id Stopping execution')
-            return
-
-        file = File.get_by_id(session, file_id = file_id)
-
-        # TODO following example from prior, cast data to b64  expected format
-
-        connection_mock : {
-            endpoint : private_host
-            }
-
-        # TODO get google vertex AI client
-        # or   with connection:  "
-        client = connection.get_client()
-
-        # TODO run it
-        response = something()
-        # Save annotations
-
-        # Call ExternalMap
-        # Bit of an odd one in mocking global attribute map.
-        mock_external_map = {
-            "negative" : {89 : {display_name: "neutral", id: 241, name: 242}},
-            }
-
-        instance_list = []
-        for doc in result:         
-            instance_list.append({
-                'type': 'global',
-                'attribute_groups': mock_external_map[doc.sentiment]
-            })
-
-        if do_save_annotations is True:
-            # For tracking and flexbility 
-            enqueue_packet(
-                session=session,
-                media_url=None,
-                media_type='text',
-                file_id=file.id,
-                instance_list=instance_list,
-                commit_input=True,
-                mode="update")
-        
-    def create_action_template():
-        Action_Template.new(
-            session = session,
-            public_name = 'TBD',
-            description = 'sss',
-            icon = '',
-            kind = '',
-            category = None,
-            #trigger_data = {'trigger_event_name': 'task_completed'},
-            #condition_data = {'event_name': 'all_tasks_completed'},
-            #completion_condition_data = {'event_name': 'prediction_success'},
-        )
+        return
+    #     file_id = self.event_data['file_id']
+    #     if not file_id:
+    #         logger.warning(f'Action has no file_id Stopping execution')
+    #         return
+    #
+    #     file = File.get_by_id(session, file_id = file_id)
+    #
+    #     # TODO following example from prior, cast data to b64  expected format
+    #
+    #     connection_mock : {
+    #         endpoint : private_host
+    #         }
+    #
+    #     # TODO get google vertex AI client
+    #     # or   with connection:  "
+    #     client = connection.get_client()
+    #
+    #     # TODO run it
+    #     response = something()
+    #     # Save annotations
+    #
+    #     # Call ExternalMap
+    #     # Bit of an odd one in mocking global attribute map.
+    #     mock_external_map = {
+    #         "negative" : {89 : {display_name: "neutral", id: 241, name: 242}},
+    #         }
+    #
+    #     instance_list = []
+    #     for doc in result:
+    #         instance_list.append({
+    #             'type': 'global',
+    #             'attribute_groups': mock_external_map[doc.sentiment]
+    #         })
+    #
+    #     if do_save_annotations is True:
+    #         pass
+    #         # For tracking and flexbility
+    #         # TODO: might need to create a consumer on walrus for this, or do an HTTP call
+    #         # enqueue_packet(
+    #         #     session=session,
+    #         #     media_url=None,
+    #         #     media_type='text',
+    #         #     file_id=file.id,
+    #         #     instance_list=instance_list,
+    #         #     commit_input=True,
+    #         #     mode="update")
+    #         #
+    # def create_action_template():
+    #     Action_Template.new(
+    #         session = session,
+    #         public_name = 'TBD',
+    #         description = 'sss',
+    #         icon = '',
+    #         kind = '',
+    #         category = None,
+    #         #trigger_data = {'trigger_event_name': 'task_completed'},
+    #         #condition_data = {'event_name': 'all_tasks_completed'},
+    #         #completion_condition_data = {'event_name': 'prediction_success'},
+    #     )
