e0b93221e807e72a1509a17858b8da8ad192b0cc
Pablo <pjestradac@gmail.com>
2022-05-26 00:38:02 -0600

    fix: startup walrus

--- walrus/main.py
+++ walrus/main.py
@@ -11,105 +11,105 @@ from shared.settings import settings
 app = Flask('Diffgram',
             static_folder = "./dist/static",
             template_folder = "./dist")
+
+start_time = time.time()
+
+# app.app_context()
+sslify = SSLify(app, subdomains = True)
+
+# This is so all the ORM can map the shared modules
+# Appears to be needed even if not directly using them
+# Maybe a setting we can look into
+# Unlikely to need all of these BUT not worth energy to figure out right now
+
+import shared.database_setup_supporting
+from methods.connectors import connector_interface
+from methods.input.upload import api_project_upload_large
+
+from methods.input.packet import input_packet
+from methods.input.input_view import input_list_web
+from methods.input.input_update import api_input_update
+from methods.input.input_view_detail import input_view_detail_api
+
+from methods.export.export_web import web_export_to_file
+from shared.export.export_generation import new_external_export
+from methods.export.export_view import export_list
+
+from methods.video.interpolation import interpolate_all_frames
+from methods.connectors.labelbox_connector import labelbox_web_hook_manager
+from methods.connectors.scale_ai_connector import send_task_to_scale_ai, task_completed_scaleai
+
+from methods.interservice.interservice_receive_api import interservice_receive_api
+from methods.data_mocking.generate_data import generate_data_api
+from methods.eventhub.eventhub_new import new_eventhub_web
+from methods.project_migration.project_migration_new import api_new_project_migration
+from methods.project_migration.project_migration_detail import api_project_migration_detail
+from methods.project_migration.project_migration_list import api_project_migration_list
+from methods import routes as routes_blueprint
+
+with app.app_context():
+    from shared.error_handlers.error_handlers import *
+
+app.register_blueprint(routes_blueprint)
+
+
+@app.route('/', methods = ['GET'])
+def walrus_alive():
+    return """
+    <img src="https://storage.googleapis.com/diffgram_public/walrus/alive.jpg" width="100%">
+    """
+
+
+@app.route('/api/walrus/status', methods = ['GET'])
+def walrus_alive_api():
+    return jsonify(True), 200
+
+
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days = 14)
+app.config['MAX_CONTENT_LENGTH'] = 250 * 1024 * 1024  # 250 Mb limit
+
+
+@app.before_request
+def handle_chunking():
+    """
+    Sets the "wsgi.input_terminated" environment flag, thus enabling
+    Werkzeug to pass chunked requests as streams.  The gunicorn server
+    should set this, but it's not yet been implemented.
+    """
+
+    transfer_encoding = request.headers.get("Transfer-Encoding", None)
+    if transfer_encoding == u"chunked":
+        request.environ["wsgi.input_terminated"] = True
+
+
+app.secret_key = settings.SECRET_KEY
+from methods.task.task_template.task_template_launch_handler import TaskTemplateLauncherThread
+from methods.sync_events.sync_actions_handler import SyncActionsHandlerThread
+from shared.helpers.security import limiter
+from methods.startup.system_startup_checker import WalrusServiceSystemStartupChecker
+from methods.input.process_media_queue_manager import ProcessMediaQueueManager
+
+limiter.init_app(app)
+
+settings.DIFFGRAM_SERVICE_NAME = 'walrus_service'
+startup_checker = WalrusServiceSystemStartupChecker()
+startup_checker.execute_startup_checks()
+
+# This starts the queue loop for processing media uploads.
+process_media_queue_manager = ProcessMediaQueueManager()
+process_media_queue_manager.start_process_media_threads()
+
+# This starts the thread for checking job launches queue.
+job_launcher_thread = TaskTemplateLauncherThread(
+    run_once = False,
+    thread_sleep_time_min = settings.TASK_TEMPLATE_THREAD_SLEEP_TIME_MIN,
+    thread_sleep_time_max = settings.TASK_TEMPLATE_THREAD_SLEEP_TIME_MAX)
+sync_actions_thread = SyncActionsHandlerThread(thread_sleep_time_min = settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MIN,
+                                               thread_sleep_time_max = settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MAX,
+                                               run_once = False)
+
+print("Startup in", time.time() - start_time)
 if __name__ == '__main__':
-
-    start_time = time.time()
-
-    # app.app_context()
-    sslify = SSLify(app, subdomains = True)
-
-    # This is so all the ORM can map the shared modules
-    # Appears to be needed even if not directly using them
-    # Maybe a setting we can look into
-    # Unlikely to need all of these BUT not worth energy to figure out right now
-
-    import shared.database_setup_supporting
-    from methods.connectors import connector_interface
-    from methods.input.upload import api_project_upload_large
-
-    from methods.input.packet import input_packet
-    from methods.input.input_view import input_list_web
-    from methods.input.input_update import api_input_update
-    from methods.input.input_view_detail import input_view_detail_api
-
-    from methods.export.export_web import web_export_to_file
-    from shared.export.export_generation import new_external_export
-    from methods.export.export_view import export_list
-
-    from methods.video.interpolation import interpolate_all_frames
-    from methods.connectors.labelbox_connector import labelbox_web_hook_manager
-    from methods.connectors.scale_ai_connector import send_task_to_scale_ai, task_completed_scaleai
-
-    from methods.interservice.interservice_receive_api import interservice_receive_api
-    from methods.data_mocking.generate_data import generate_data_api
-    from methods.eventhub.eventhub_new import new_eventhub_web
-    from methods.project_migration.project_migration_new import api_new_project_migration
-    from methods.project_migration.project_migration_detail import api_project_migration_detail
-    from methods.project_migration.project_migration_list import api_project_migration_list
-    from methods import routes as routes_blueprint
-
-    with app.app_context():
-        from shared.error_handlers.error_handlers import *
-
-    app.register_blueprint(routes_blueprint)
-
-
-    @app.route('/', methods = ['GET'])
-    def walrus_alive():
-        return """
-    	<img src="https://storage.googleapis.com/diffgram_public/walrus/alive.jpg" width="100%">
-    	"""
-
-
-    @app.route('/api/walrus/status', methods = ['GET'])
-    def walrus_alive_api():
-        return jsonify(True), 200
-
-
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days = 14)
-    app.config['MAX_CONTENT_LENGTH'] = 250 * 1024 * 1024  # 250 Mb limit
-
-
-    @app.before_request
-    def handle_chunking():
-        """
-        Sets the "wsgi.input_terminated" environment flag, thus enabling
-        Werkzeug to pass chunked requests as streams.  The gunicorn server
-        should set this, but it's not yet been implemented.
-        """
-
-        transfer_encoding = request.headers.get("Transfer-Encoding", None)
-        if transfer_encoding == u"chunked":
-            request.environ["wsgi.input_terminated"] = True
-
-
-    app.secret_key = settings.SECRET_KEY
-    from methods.task.task_template.task_template_launch_handler import TaskTemplateLauncherThread
-    from methods.sync_events.sync_actions_handler import SyncActionsHandlerThread
-    from shared.helpers.security import limiter
-    from methods.startup.system_startup_checker import WalrusServiceSystemStartupChecker
-    from methods.input.process_media_queue_manager import ProcessMediaQueueManager
-
-    limiter.init_app(app)
-
-    settings.DIFFGRAM_SERVICE_NAME = 'walrus_service'
-    startup_checker = WalrusServiceSystemStartupChecker()
-    startup_checker.execute_startup_checks()
-
-    # This starts the queue loop for processing media uploads.
-    process_media_queue_manager = ProcessMediaQueueManager()
-    process_media_queue_manager.start_process_media_threads()
-
-    # This starts the thread for checking job launches queue.
-    job_launcher_thread = TaskTemplateLauncherThread(
-        run_once = False,
-        thread_sleep_time_min = settings.TASK_TEMPLATE_THREAD_SLEEP_TIME_MIN,
-        thread_sleep_time_max = settings.TASK_TEMPLATE_THREAD_SLEEP_TIME_MAX)
-    sync_actions_thread = SyncActionsHandlerThread(thread_sleep_time_min = settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MIN,
-                                                   thread_sleep_time_max = settings.SYNC_ACTIONS_THREAD_SLEEP_TIME_MAX,
-                                                   run_once = False)
-
-    print("Startup in", time.time() - start_time)
 
     settings.NAME_EQUALS_MAIN = True  # can adjust this for local deferral testing if needed?
     limiter.enabled = False
@@ -120,8 +120,6 @@ if __name__ == '__main__':
     # code below app.run will not execute!!!
 else:
     print("settings.NAME_EQUALS_MAIN", settings.NAME_EQUALS_MAIN)
-    import shared.database_setup_supporting
 
-    app.debug = False
-    # This line is needed in order to run the server behind Gunicorn
+    app.debug == False
     app.wsgi_app = ProxyFix(app.wsgi_app, num_proxies = 2)
