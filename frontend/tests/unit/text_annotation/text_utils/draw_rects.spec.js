5a25ea57e5ca736a866fa2dfa18822dc3bbee193
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-30 11:47:51 -0500

    Add tests for rendering rects on multiple lines

--- frontend/tests/unit/text_annotation/text_utils/draw_rects.spec.js
+++ frontend/tests/unit/text_annotation/text_utils/draw_rects.spec.js
@@ -24,7 +24,33 @@ describe("Test DrawRects class (draw_rects.ts)", () => {
     it("Should return one rect with the start coordinate of end token and width that covers all the selected tokens if the tokens are on the same line (end token < start token)", () => {
         const rects = draw_rects.generate_rects(tokens[3].id, tokens[1].id)
 
-        expect(rects[0].x).toEqual(tokens[0].start_x)
-        expect(rects[0].width).toEqual(tokens[3].start_x + tokens[3].width - tokens[0].start_x)
+        expect(rects[0].x).toEqual(tokens[1].start_x)
+        expect(rects[0].width).toEqual(tokens[3].start_x + tokens[3].width - tokens[1].start_x)
+    })
+
+    it("Should return array of rects equal to the humber of line that selection belongs to", () => {
+        const start_token = tokens[2]
+        const first_line_last_token = tokens.filter(token => token.line === 0).pop()
+
+        const second_line_tokens = tokens.filter(token => token.line === 1)
+        const seond_line_token_count = second_line_tokens.length
+
+        const third_line_start_token = tokens.find(token => token.line === 2)
+        const end_token = tokens.filter(token => token.line === 2)[2]
+
+        const rects = draw_rects.generate_rects(start_token.id, end_token.id)
+
+        const start_line_rect = rects[0]
+        const mid_line_rect = rects[1]
+        const end_line_rect = rects[2]
+
+        expect(start_line_rect.x).toEqual(start_token.start_x)
+        expect(start_line_rect.width).toEqual(first_line_last_token.start_x + first_line_last_token.width - start_token.start_x)
+
+        expect(second_line_tokens[0].x).toEqual(mid_line_rect.start_x)
+        expect(mid_line_rect.width).toEqual(second_line_tokens[seond_line_token_count - 1].start_x + second_line_tokens[seond_line_token_count - 1].width - second_line_tokens[0].start_x)
+
+        expect(end_line_rect.x).toEqual(third_line_start_token.start_x)
+        expect(end_line_rect.width).toEqual(end_token.start_x + end_token.width - third_line_start_token.start_x)
     })
 })
