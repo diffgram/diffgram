3c822180488947ba679ebde4797c1c08939436a0
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-30 11:13:49 -0500

    Add tests for the rects calculation if the token are on the same line

--- frontend/tests/unit/text_annotation/text_utils/draw_rects.spec.js
+++ frontend/tests/unit/text_annotation/text_utils/draw_rects.spec.js
@@ -13,4 +13,18 @@ describe("Test DrawRects class (draw_rects.ts)", () => {
         expect(rects[0].x).toEqual(tokens[0].start_x)
         expect(rects[0].width).toEqual(tokens[0].width)
     })
+
+    it("Should return one rect with the start coordinate of start token and width that covers all the selected tokens if the tokens are on the same line (start token < end token)", () => {
+        const rects = draw_rects.generate_rects(tokens[0].id, tokens[3].id)
+
+        expect(rects[0].x).toEqual(tokens[0].start_x)
+        expect(rects[0].width).toEqual(tokens[3].start_x + tokens[3].width - tokens[0].start_x)
+    })
+
+    it("Should return one rect with the start coordinate of end token and width that covers all the selected tokens if the tokens are on the same line (end token < start token)", () => {
+        const rects = draw_rects.generate_rects(tokens[3].id, tokens[1].id)
+
+        expect(rects[0].x).toEqual(tokens[0].start_x)
+        expect(rects[0].width).toEqual(tokens[3].start_x + tokens[3].width - tokens[0].start_x)
+    })
 })
