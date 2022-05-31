65fcfc8b04b72acc8c0c5ecec7da9e40a7fa478b
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-05-31 10:05:09 -0500

    Add tests for on_apply_label function in fast menu

--- frontend/tests/unit/text_annotation/render_elements/fast_label_menu.spec.js
+++ frontend/tests/unit/text_annotation/render_elements/fast_label_menu.spec.js
@@ -21,15 +21,20 @@ describe("Tests for fast_label_menu.vue", () => {
         },
     ]
 
-    it("Should be mounted succssfufully with rects prop", () => {
-        const rects = [
-            {
-                x: 10,
-                y:10,
-                width: 20
-            }
-        ]
+    const rects = [
+        {
+            x: 10,
+            y:10,
+            width: 20
+        }
+    ]
 
+    const arrow_position = {
+        x: 10,
+        y: 10
+    }
+
+    it("Should be mounted succssfufully with rects prop", () => {
         const props = {
             propsData: {
                 label_list,
@@ -39,5 +44,45 @@ describe("Tests for fast_label_menu.vue", () => {
 
         const wrapper = shallowMount(fast_label_menu, props)
         expect(wrapper.html()).toContain('style="top: 60px; left: 390px;"')
+    })
+
+    it("Should be mounted succssfufully with arrow_position prop", () => {
+        const props = {
+            propsData: {
+                label_list,
+                arrow_position
+            }
+        }
+
+        const wrapper = shallowMount(fast_label_menu, props)
+        expect(wrapper.html()).toContain('style="top: 35px; left: 360px;"')
+    })
+
+    it("on_apply_label should emit create_instance event if rects props is provided", () => {
+        const props = {
+            propsData: {
+                label_list,
+                rects
+            }
+        }
+
+        const wrapper = shallowMount(fast_label_menu, props)
+        wrapper.vm.on_apply_label(label_list[0])
+
+        expect(wrapper.emitted('create_instance')).toBeTruthy()
+    })
+
+    it("on_apply_label should emit create_instance event if rects arrow_position is provided", () => {
+        const props = {
+            propsData: {
+                label_list,
+                arrow_position
+            }
+        }
+
+        const wrapper = shallowMount(fast_label_menu, props)
+        wrapper.vm.on_apply_label(label_list[0])
+
+        expect(wrapper.emitted('create_relation')).toBeTruthy()
     })
 })
