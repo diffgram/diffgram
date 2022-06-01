419dae18319a6f71738bfaf4c243672070d26cd2
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-01 10:34:24 -0500

    Add file for tests of test sidebar

---
+++ frontend/tests/unit/text_annotation/text_sidebar.spec.js
@@ -0,0 +1,8 @@
+import text_sidebar from "../../../src/components/text_annotation/text_sidebar.vue"
+import { shallowMount } from "@vue/test-utils";
+
+describe("text_sidebar.vue", () => {
+    it("Runs test sideba tests", () => {
+        console.log("here")
+    })
+})
