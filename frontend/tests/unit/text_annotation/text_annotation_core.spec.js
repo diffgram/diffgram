60810a0eb08a317ad913c39a852f64a6bdb09fbf
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-02 10:35:11 -0500

    Initialize text interface with proper props

--- frontend/tests/unit/text_annotation/text_annotation_core.spec.js
+++ frontend/tests/unit/text_annotation/text_annotation_core.spec.js
@@ -6,8 +6,23 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe("text_annotation_core.vue", () => {
+    let props;
+
+    beforeEach(() => {
+        props = {
+            propsData: {
+                label_file_colour_map: {},
+                label_list: [],
+                project_string_id: "project_string_id",
+                global_attribute_groups_list: [],
+                per_instance_attribute_groups_list: [],
+                label_schema: 1
+            }
+        }
+    })
+
     it("Renders text interface loading state properly", () => {
-        const wrapper = shallowMount(text_annotation_core, {}, localVue)
+        const wrapper = shallowMount(text_annotation_core, props, localVue)
         expect(wrapper.text()).toEqual("Loading...")
     })
 })
