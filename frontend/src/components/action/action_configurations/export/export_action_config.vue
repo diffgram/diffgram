610d0f0e9b22e96d985bf243a35549a5415315ef
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-06-22 10:19:59 -0500

    Add new steps handler to the export action

--- frontend/src/components/action/action_configurations/export/export_action_config.vue
+++ frontend/src/components/action/action_configurations/export/export_action_config.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container fluid style="height: 100%">
     <action_config_base
+      v-if="steps_config"
       :project_string_id="project_string_id"
       :display_mode="display_mode"
       @open_action_selector="$emit('open_action_selector')"
+      :steps_config="steps_config.generate()"
       :actions_list="actions_list"
       :action="action">
 
@@ -100,6 +102,7 @@ import pre_conditions_config from "@/components/action/actions_config_base/pre_c
 import action_config_base from "@/components/action/actions_config_base/action_config_base";
 import complete_conditions_config from "@/components/action/actions_config_base/complete_conditions_config";
 import action_config_mixin from "../action_config_mixin";
+import ActionStepsConfig from '../ActionStepsConfig';
 
 export default {
   name: "export_action_config",
@@ -112,11 +115,9 @@ export default {
     complete_conditions_config,
     directory_list: directory_list
   },
-  props:{
-
-  },
   data: function(){
     return{
+      steps_config: null,
       completion_condition_list_prop: [
         {
           name: 'Export Finishes Generating',
@@ -181,6 +182,9 @@ export default {
         }
       ]
     }
+  },
+  mounted() {
+    this.steps_config = new ActionStepsConfig()
   },
 
   computed:{
