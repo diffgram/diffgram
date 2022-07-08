105f1f02d4178eafdd54020f16b515086b283a95
Vitalii Bulyzhyn <vitaliibulyzhyn@Vitaliis-MacBook-Pro.local>
2022-07-08 00:27:15 -0500

    Add route to get previus action by action id

---
+++ default/methods/action/action_previous.py
@@ -0,0 +1,24 @@
+from methods.regular.regular_api import *
+from shared.database.action.action import Action
+from shared.regular.regular_log import log_has_error
+
+@routes.route('/api/v1/project/<string:project_string_id>/action/previous/<string:action_id>', methods=['GET'])
+@Project_permissions.user_has_project(
+    Roles=["admin", "Editor"],
+    apis_user_list=['api_enabled_builder', 'security_email_verified'])
+@limiter.limit("20 per day")
+def api_action_previous(project_string_id, action_id):
+
+    with sessionMaker.session_scope() as session:
+        log = regular_log.default()
+
+        project = Project.get(session, project_string_id)
+        action = Action.get_by_id(session = session, id = action_id, project_id = project.id)
+        
+        previous_action = action.get_previous_action(session=session).serialize()
+
+        if log_has_error(log) >= 1:
+            return jsonify(log = log), 400
+
+        out = jsonify(previuos_action=previous_action, log=log)
+        return out, 200
