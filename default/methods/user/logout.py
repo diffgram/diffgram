8dbb35394e2a95bce427c1ec6cd9164f2eabfd5a
PJEstrada <pjestrada006@hotmail.com>
2022-07-12 11:26:16 -0600

    Cognito Token Storage Size & Login Optimizations (#971)
    * refactor refresh token storage
    
    * fix: logout process
    
    Co-authored-by: Pablo <pjestradac@gmail.com>

--- default/methods/user/logout.py
+++ default/methods/user/logout.py
@@ -8,9 +8,8 @@ from shared.helpers.permissions import get_decoded_jwt_from_session
 
 def oauth2_logout() -> [dict, int]:
     oauth2 = OAuth2Provider()
-    jwt_data = get_decoded_jwt_from_session()
+    refresh_token = get_decoded_jwt_from_session()
     oauth_client = oauth2.get_client()
-    refresh_token = oauth_client.get_refresh_token_from_jwt(jwt_data = jwt_data)
     login_session['jwt'] = None
     url_data = oauth_client.logout(refresh_token = refresh_token)
     return {"url_redirect": url_data}, 200
