f0ed0181d23238bfe33e6747755d839a9f50c7bc
PJEstrada <pjestrada006@hotmail.com>
2022-07-25 14:05:25 -0600

    Custom API Signer (#998)
    * feat: add custom url signer
    
    * feat: add custom url signer
    
    * feat: bypass connection
    
    * fix: alembic versions
    
    * fix: use get params
    
    Co-authored-by: Pablo <pjestradac@gmail.com>

--- shared/url_generation.py
+++ shared/url_generation.py
@@ -61,11 +61,10 @@ def get_url_from_connector(connector, params, log):
     :return:
     """
     connector.connect()
-    print('params', params)
     response = connector.fetch_data(params)
     if response is None or response.get('result') is None:
         msg = f'Error regenerating URL: {params}. Response: {response}'
-        log['error']['connector_s3_client'] = msg
+        log['error']['connector_client'] = msg
         logger.error(msg)
         return None, log
     signed_url = response.get('result')
@@ -183,7 +182,7 @@ def blob_regenerate_url(blob_object: DiffgramBlobObjectType,
         )
 
     if strategy == "connection":
-        logger.info(f'Generate Signed Url with connection {connection_id} on bucket {bucket_name}')
+        logger.debug(f'Generate Signed Url with connection {connection_id} on bucket {bucket_name}')
         blob_object, log = connection_url_regenerate(
             session = session,
             blob_object = blob_object,
