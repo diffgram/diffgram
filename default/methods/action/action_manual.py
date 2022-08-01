aab08d035c2167180523229da4397c5706bb1532
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2022-08-01 12:22:56 -0700

    Add security check
    otherwise it's blindly trusting provided ids
    fix 200 ok

--- default/methods/action/action_manual.py
+++ default/methods/action/action_manual.py
@@ -1,11 +1,13 @@
 from methods.regular.regular_api import *
 
+from shared.database.action.action import Action
+from shared.database.action.workflow import Workflow
+
 @routes.route('/api/v1/project/<string:project_string_id>/workflow/<int:workflow_id>/actions/<int:action_id>/manual_trigger',
               methods = ['PUT'])
 @Project_permissions.user_has_project(
     Roles = ["admin", "Editor"],
     apis_user_list = ['api_enabled_builder', 'security_email_verified'])
-@limiter.limit("20 per day")
 def api_action_manual(project_string_id, workflow_id, action_id):
     """
 
@@ -13,6 +15,12 @@ def api_action_manual(project_string_id, workflow_id, action_id):
     with sessionMaker.session_scope() as session:
         project = Project.get(session, project_string_id)
         member = get_member(session)
+
+        action = Action.get_by_id(session, action_id, project.id)
+        if action is None: return jsonify("bad action id"), 403 
+
+        if action.workflow_id != workflow_id:
+            return jsonify("bad workflow_id id"), 403 
 
         Event.new(
             session = session,
@@ -23,4 +31,4 @@ def api_action_manual(project_string_id, workflow_id, action_id):
             workflow_id = workflow_id
         )
 
-        return jsonify()
+        return jsonify(), 200
