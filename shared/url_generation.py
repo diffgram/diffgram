dcb88a5b129d4f5d9073baa7223b8edffca8b533
Pablo <pjestradac@gmail.com>
2022-08-04 15:45:17 -0600

    fix: avoid saving urls on files with connection id

--- shared/url_generation.py
+++ shared/url_generation.py
@@ -315,14 +315,14 @@ def blob_regenerate_url(blob_object: DiffgramBlobObjectType,
     if not blob_object.url_signed_blob_path:
         return
 
-    should_regenerate, new_offset_in_seconds = data_tools.determine_if_should_regenerate_url(blob_object, session)
-
-    if should_regenerate is not True:
-        return
-
     strategy = determine_url_regenerate_strategy(
         connection_id = connection_id,
         bucket_name = bucket_name)
+
+    should_regenerate, new_offset_in_seconds = data_tools.determine_if_should_regenerate_url(blob_object, session)
+
+    if should_regenerate is not True and strategy != 'connection':
+        return
 
     logger.debug(f"Regenerating with {strategy} strategy")
     if strategy == "default":
