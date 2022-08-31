ba1534af491ebc704bec6092b3cc33e2e3e344d8
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-08-31 18:15:29 -0300

    Add popup with canvas overlay on click

--- frontend/src/components/source_control/map_explorer.vue
+++ frontend/src/components/source_control/map_explorer.vue
@@ -11,11 +11,25 @@
             ref="map" 
             :style="`height: calc(100vh - 48px); z-index: 0; width: 100%;`"
         />
+        <div class="ol-popup" id="popup" ref="popup">
+            <file_preview
+                class="file-preview"
+                v-if="file_list && file_list.length > 0"
+                :key="file_list[1].id"
+                :project_string_id="project_string_id"
+                :file="file_list[1]"
+                :instance_list="file_list[1].instance_list"
+                :show_ground_truth="true"
+            />
+        </div>
     </div>
 </template>
 
 <script>
 import Vue from "vue";
+import axios from "axios";
+import file_preview from "./file_preview"
+
 import Map from 'ol/Map';
 import TileLayer from 'ol/layer/WebGLTile';
 import OSM from 'ol/source/OSM';
@@ -25,9 +39,13 @@ import Point from 'ol/geom/Point'
 import VectorLayer from 'ol/layer/Vector'
 import VectorSource from 'ol/source/Vector'
 import { Circle as CircleStyle, Fill, Stroke, Style } from 'ol/style';
+import Overlay from 'ol/Overlay';
 
 export default Vue.extend({
     name: 'map_explorer',
+    components: {
+        file_preview
+    },
     props: {
         project_string_id: {
             type: String,
@@ -45,7 +63,19 @@ export default Vue.extend({
     data() {
         return {
             map: null,
-            render_source: null
+            render_source: null,
+            overlay: null,
+            file_list: [],
+            metadata: {
+                'directory_id': undefined,
+                'limit': 28,
+                'media_type': this.filter_media_type_setting,
+                'page': 1,
+                'query_menu_open' : false,
+                'file_view_mode': 'explorer',
+                'previous': undefined,
+                'search_term': this.search_term
+                }
         }
     },
     mounted() {
@@ -73,7 +103,9 @@ export default Vue.extend({
                 ]
             });
 
-            this.map = map 
+            this.map = map
+            this.handle_popup_open()
+            this.fetch_file_list()
         },
         place_items_on_map: function() {
             // WIP: this function should get data from the backend and place it to the map
@@ -97,7 +129,124 @@ export default Vue.extend({
             const pointFeature = new Feature(new Point([-109.89, 45.02]));
             pointFeature.setStyle(style)
             this.render_source.addFeature(pointFeature)
+        },
+        handle_popup_open: function() {
+            const container = document.getElementById('popup');
+
+            const overlay = new Overlay({
+                element: container,
+                autoPan: {
+                    animation: {
+                    duration: 250,
+                    },
+                },
+            });
+
+            this.map.addOverlay(overlay)
+            this.map.on('singleclick', function (evt) {
+                const coordinate = evt.coordinate;
+
+                overlay.setPosition(coordinate);
+            });
+        },
+        fetch_file_list: async function(reload_all = true){
+        if(reload_all){
+          this.metadata.page = 1;
+          this.loading = true
         }
+        else{
+          this.infinite_scroll_loading = true;
+        }
+        try{
+          this.none_found = undefined
+          const response = await axios.post('/api/project/' + String(this.$props.project_string_id) +
+            '/user/' + this.$store.state.user.current.username + '/file/list', {
+            'metadata': {
+              ...this.metadata,
+              query: this.query,
+              previous: this.metadata_previous
+            },
+            'project_string_id': this.$props.project_string_id
+
+          })
+          if (response.data['file_list'] == false) {
+            this.none_found = true
+          }
+          else {
+            if(reload_all){
+              this.file_list = response.data.file_list;
+            }
+            else{
+
+              for(const file in response.data.file_list){
+                if(!this.file_list.find(f => f.id === file.id)){
+                  this.file_list.push(file);
+                }
+              }
+              this.file_list = this.file_list.concat(response.data.file_list);
+            }
+
+          }
+          this.metadata_previous = response.data.metadata;
+        }
+        catch (error) {
+          console.error(error);
+          this.query_error = this.$route_api_errors(error)
+        }
+        finally {
+          if(reload_all){
+            this.loading = false;
+          }
+          else{
+            this.infinite_scroll_loading = false;
+          }
+
+        }
+      },
     }
 })
 </script>
+
+<style scoped>
+      .ol-popup {
+        position: absolute;
+        background-color: white;
+        box-shadow: 0 1px 4px rgba(0,0,0,0.2);
+        padding: 15px;
+        border-radius: 10px;
+        border: 1px solid #cccccc;
+        bottom: 12px;
+        left: -50px;
+        min-width: 280px;
+      }
+      .ol-popup:after, .ol-popup:before {
+        top: 100%;
+        border: solid transparent;
+        content: " ";
+        height: 0;
+        width: 0;
+        position: absolute;
+        pointer-events: none;
+      }
+      .ol-popup:after {
+        border-top-color: white;
+        border-width: 10px;
+        left: 48px;
+        margin-left: -10px;
+      }
+      .ol-popup:before {
+        border-top-color: #cccccc;
+        border-width: 11px;
+        left: 48px;
+        margin-left: -11px;
+      }
+      .ol-popup-closer {
+        text-decoration: none;
+        position: absolute;
+        top: 2px;
+        right: 8px;
+      }
+      .ol-popup-closer:after {
+        content: "✖";
+      }
+</style>
