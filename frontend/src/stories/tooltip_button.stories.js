9a9d867f85253e83b1e987d4eabc0ab6b142070d
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-09-09 16:33:07 -0300

    Add vuetify plugin to the storybook and add tooltip button story

---
+++ frontend/src/stories/tooltip_button.stories.js
@@ -0,0 +1,24 @@
+import tooltip_button from '../components/regular/tooltip_button.vue';
+
+export default {
+  title: 'Regular/tooltip_button',
+  component: tooltip_button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+    },
+  },
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { tooltip_button },
+  template: '<tooltip_button v-bind="$props" />',
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
