522ce08200f59e9c1ab07a651135f289f510df65
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-09-12 11:53:29 -0300

    Clean up stories

--- frontend/src/stories/tooltip_button.stories.js
+++ frontend/src/stories/tooltip_button.stories.js
@@ -1,24 +1,14 @@
 import tooltip_button from '../components/regular/tooltip_button.vue';
 
 export default {
-  title: 'Regular/tooltip_button',
-  component: tooltip_button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-    size: {
-      control: { type: 'select' },
-    },
-  },
+  title: 'Regular/TooltipButton',
+  component: tooltip_button
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { tooltip_button },
+export const TooltipButton = () => ({
+  components: { 
+    tooltip_button 
+  },
   template: '<tooltip_button v-bind="$props" />'
 });
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
