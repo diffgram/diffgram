0dda22d8fc91408a50cc0d96b4652838abdfabb7
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-09-12 12:52:55 -0300

    Refactor storybook store handler

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,12 +1,14 @@
 import { withVuetify } from '@socheatsok78/storybook-addon-vuetify/dist/decorators'
 import store from '../src/store'
 
+const withStore = () => ({ 
+  template: "<story />",
+  store 
+})
+
 export const decorators = [
   withVuetify,
-  () => ({ 
-    template: "<story />",
-    store 
-  })
+  withStore
 ]
 
 export const parameters = {
