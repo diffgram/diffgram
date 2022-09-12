16a0c677c2e524d48e74f78e5f6763783dcf4648
Pablo <pjestradac@gmail.com>
2022-09-12 12:38:54 -0600

    fix: migrations and cancel requests

--- frontend/src/services/customInstance/index.js
+++ frontend/src/services/customInstance/index.js
@@ -5,11 +5,15 @@ const axiosConfig = {
   baseURL: 'http://127.0.0.1:8085/diffgrampower'
 }
 const instance = axios.create()
-
+instance.CancelToken = axios.CancelToken;
+instance.isCancel = axios.isCancel;
 instance.interceptors.response.use(function (response) {
     return response;
   }, async function (error) {
-      const { status } = error.response
+      if(error.response){
+        const { status } = error.response
+      }
+
     if (status === 401) {
         await logout()
     }
