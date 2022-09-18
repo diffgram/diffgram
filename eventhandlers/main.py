7c4f84d8cbe7eee1a97697fc2bcd9f4b2eca76f8
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-09-18 18:30:34 -0300

    Add updateing running actions status of the service is shouted down

--- eventhandlers/main.py
+++ eventhandlers/main.py
@@ -1,10 +1,14 @@
 from shared.database_setup_supporting import *
 from shared.shared_logger import get_shared_logger
+from shared.database.action.action_run import ActionRun
 from ConsumersCreator import ConsumerCreator
 from shared.settings import settings
 from startup.system_startup_checker import EventHandlerSystemStartupChecker
 from action_runners.ActionRegistrar import register_all
+from shared.helpers import sessionMaker
 import traceback
+import atexit
+
 logger = get_shared_logger()
 
 startup_checker = EventHandlerSystemStartupChecker()
@@ -22,3 +26,9 @@ try:
 except Exception as e:
     data = traceback.format_exc()
     logger.error(f'Error Processing event: {data}')
+
+def exit_handler():
+    with sessionMaker.session_scope_threaded() as session:
+        ActionRun.shout_down_running_actions(session)
+
+atexit.register(exit_handler)
