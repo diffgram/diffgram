febe7863769f1c88e79c7de9d81b587611205d7c
Pablo <pjestrada006@hotmail.com>
2022-09-23 07:38:28 -0600

    fix: bump usage limits

--- shared/helpers/security.py
+++ shared/helpers/security.py
@@ -4,6 +4,6 @@ from flask_limiter.util import get_remote_address
 from flask import request
 
 limiter = Limiter(  key_func = get_remote_address,
-					default_limits = ["60000 per day", "9000 per hour", "100 per second"],
+					default_limits = ["6000000 per day", "900000 per hour", "10000 per second"],
 					headers_enabled = True
 					)
