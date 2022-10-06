7fc8105f4c744ca15205814f3c8fb93c6a8bfe7f
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-10-06 14:32:42 -0300

    Fix us shema request

--- frontend/src/components/ui_schema/ui_schema.ts
+++ frontend/src/components/ui_schema/ui_schema.ts
@@ -1,30 +1,32 @@
 import {v4 as uuidv4 } from 'uuid'
-
 export class UI_Schema {
-  public client_creation_ref_id
-  public client_created_time
-  public name
-  public archived
-  public is_visible
-  public is_public
+  public client_creation_ref_id: string
+  public client_created_time: string
+  public name: string
+  public archived: boolean
+  public is_visible: boolean
+  public is_public: boolean
 
-  constructor() {
-      
-  }
-
-  public new(): any {
-
+  public new(): void {
     this.client_creation_ref_id = uuidv4();
     this.client_created_time = new Date().toISOString();
     this.name = "Untitled";
     this.archived = false
     this.is_visible = true
     this.is_public = false
-
   }
 
-  public serialize(): any {
-    return JSON.stringify(this)
+  public export(): object {
+    const ui_schema_object = {
+      client_creation_ref_id: this.client_creation_ref_id,
+      client_created_time: this.client_created_time,
+      name: this.name,
+      archived: this.archived,
+      is_visible: this.is_visible,
+      is_public: this.is_public
+    }
+
+    return ui_schema_object
   }
 
   public copy(old): any {
