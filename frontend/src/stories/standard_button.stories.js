f326a50ba32936b0836e004b837bf5c8dcf4f520
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-10-07 17:21:59 -0300

    Add stories for the standard button

--- frontend/src/stories/standard_button.stories.js
+++ frontend/src/stories/standard_button.stories.js
@@ -5,10 +5,50 @@ export default {
   component: standard_button,
 };
 
-export const StandardButton = (args, { argTypes }) => ({
+export const Default = (args, { argTypes }) => ({
   components: {
     standard_button,
   },
   props: Object.keys(argTypes),
   template: '<standard_button v-bind="$props" />',
 });
+
+export const Primary = (args, { argTypes }) => ({
+  components: {
+    standard_button,
+  },
+  props: Object.keys(argTypes),
+  template: '<standard_button v-bind="$props" />',
+});
+Primary.args = {
+  button_message: "Primary",
+  button_color: "primary",
+  icon: "",
+};
+
+export const PrimaryWithTooltip = (args, { argTypes }) => ({
+  components: {
+    standard_button,
+  },
+  props: Object.keys(argTypes),
+  template: '<standard_button v-bind="$props" />',
+});
+PrimaryWithTooltip.args = {
+  button_message: "Primary",
+  tooltip_message: "Primary button tooltip message",
+  button_color: "primary",
+  icon: "",
+};
+
+export const Text = (args, { argTypes }) => ({
+  components: {
+    standard_button,
+  },
+  props: Object.keys(argTypes),
+  template: '<standard_button v-bind="$props" />',
+});
+Text.args = {
+  text_style: true,
+  button_message: "Text style button",
+  icon: "",
+};
