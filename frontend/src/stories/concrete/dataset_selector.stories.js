a500e7d5f168a105b5829570c5b3bb0dabd905d7
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-10-11 15:04:40 -0300

    Create story for dataset slector

--- frontend/src/stories/concrete/dataset_selector.stories.js
+++ frontend/src/stories/concrete/dataset_selector.stories.js
@@ -2,7 +2,7 @@ import dataset_selector from "../../components/concrete/dataset/dataset_selector
 
 export default {
   title: "Concrete/dataset_selector",
-  component: dataset_selector,
+  component: dataset_selector
 };
 
 export const Default = (args, { argTypes }) => ({
@@ -12,3 +12,13 @@ export const Default = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   template: '<dataset_selector v-bind="$props" />',
 });
+Default.args = {
+  dataset_list: [
+    { directory_id: '1', nickname: 'Dataset 1', created_time:"2022-10-10T19:12:44.257372" },
+    { directory_id: '2', nickname: 'Dataset 2', created_time:"2022-10-10T19:12:44.257372" },
+    { directory_id: '3', nickname: 'Dataset 3', created_time:"2022-10-10T19:12:44.257372" },
+    { directory_id: '4', nickname: 'Dataset 4', created_time:"2022-10-10T19:12:44.257372" },
+    { directory_id: '5', nickname: 'Dataset 5', created_time:"2022-10-10T19:12:44.257372" },
+    { directory_id: '6', nickname: 'Dataset 6', created_time:"2022-10-10T19:12:44.257372" },
+  ],
+};
