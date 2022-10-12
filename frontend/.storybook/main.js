f81656d1775a77e067111723e8f2bf31bcdafe49
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-10-12 12:40:52 -0300

    Add apis mock to the storybook

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,15 +1,11 @@
 const path = require('path')
 
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "storybook-addon-mock",
     "@storybook/theming",
   ],
   framework: "@storybook/vue",
--- frontend/src/stories/concrete/dataset_selector.stories.js
+++ frontend/src/stories/concrete/dataset_selector.stories.js
@@ -2,7 +2,27 @@ import dataset_selector from "../../components/concrete/dataset/dataset_selector
 
 export default {
   title: "Concrete/dataset_selector",
-  component: dataset_selector
+  component: dataset_selector,
+  parameters: {
+    mockData: [
+      {
+        url: '/api/v1/project/project_string_id/directory/new',
+        method: 'POST',
+        status: 200,
+        response: (request) => {
+          const request_body = JSON.parse(request.body)
+          return {
+              log: {
+                success: true,
+              },
+              new_directory: {
+                nickname: request_body.nickname
+              }
+          }
+        }
+      }
+    ]
+  }
 };
 
 export const Default = (args, { argTypes }) => ({
@@ -13,6 +33,8 @@ export const Default = (args, { argTypes }) => ({
   template: '<dataset_selector v-bind="$props" />',
 });
 Default.args = {
+  project_string_id: 'project_string_id',
+  show_new: true,
   dataset_list: [
     { directory_id: '1', nickname: 'Dataset 1', created_time:"2022-10-10T19:12:44.257372" },
     { directory_id: '2', nickname: 'Dataset 2', created_time:"2022-10-10T19:12:44.257372" },
