b895dbd926545bb6ed83dcc4f1e1b6b6eab61bc9
Pablo <pjestrada006@hotmail.com>
2022-11-22 07:55:31 -0600

    fix: avoid creating thumbnails once generated

--- frontend/src/components/annotation/media_core.vue
+++ frontend/src/components/annotation/media_core.vue
@@ -1396,7 +1396,7 @@ import Compound_file_preview from "../source_control/compound_file_preview.vue";
       if(!file){
         return
       }
-      let create_thumbnails = this.context === 'task' ? false : true
+      let create_thumbnails = this.context === 'task' || file.image.url_signed_blob_path   ? false : true
       let [url_data, err] = await get_file_signed_url(project_string_id, file.id, create_thumbnails);
       if (err){
         this.error = this.$route_api_errors(err)
