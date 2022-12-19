eafffb6c459ddc804cd4fe19ddb53efbf2d9dca2
Pablo <pjestrada006@hotmail.com>
2022-12-19 10:01:22 -0600

    fix: defaults

--- shared/settings/settings.py
+++ shared/settings/settings.py
@@ -188,8 +188,8 @@ RABBITMQ_PORT = int(os.getenv('RABBITMQ_PORT', 5672))
 RABBITMQ_USE_SSL = env_adapter.bool(os.getenv('RABBITMQ_USE_SSL', False))
 
 
-SIGNED_URL_CACHE_MINIMUM_SECONDS_VALID = int(os.getenv('SIGNED_URL_CACHE_MINIMUM_SECONDS_VALID', 5))     # this should always be lower then new offset
-SIGNED_URL_CACHE_NEW_OFFSET_SECONDS_VALID = int(os.getenv('SIGNED_URL_CACHE_NEW_OFFSET_SECONDS_VALID', 6))
+SIGNED_URL_CACHE_MINIMUM_SECONDS_VALID = int(os.getenv('SIGNED_URL_CACHE_MINIMUM_SECONDS_VALID', 5*3600*24))     # this should always be lower then new offset
+SIGNED_URL_CACHE_NEW_OFFSET_SECONDS_VALID = int(os.getenv('SIGNED_URL_CACHE_NEW_OFFSET_SECONDS_VALID', 6*3600*24))
 
 # OIDC Settings
 USE_OAUTH2 = env_adapter.bool(os.getenv('USE_OAUTH2', False))
