a1a9679ced95b931860bf56811aac729754dd673
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2022-12-30 11:49:22 +0100

    Cleanup global attributes component

--- frontend/src/components/attribute/global_attributes_list.vue
+++ frontend/src/components/attribute/global_attributes_list.vue
@@ -12,10 +12,13 @@
     :tile="true"
   >
     <v-expansion-panel>
-
       <v-expansion-panel-header class="d-flex justify-start pa-0 sidebar-accordeon-header">
-
-        <v-icon left class="ml-5 flex-grow-0" color="primary" size="18">
+        <v-icon 
+          left 
+          class="ml-5 flex-grow-0" 
+          color="primary" 
+          size="18"
+        >
           mdi-file
         </v-icon>
 
@@ -23,93 +26,94 @@
 
         <v-spacer></v-spacer>
 
-        <v-chip x-small class="d-flex justify-center flex-grow-0">
-            {{global_attribute_groups_list.length}}
+        <v-chip 
+          x-small 
+          class="d-flex justify-center flex-grow-0"
+        >
+            {{ global_attribute_groups_list.length }}
         </v-chip>
       </v-expansion-panel-header>
-
+      
       <v-expansion-panel-content>
         <attribute_group_list
+          v-if="current_global_instance && global_attribute_groups_list && global_attribute_groups_list.length != 0"
           style="overflow-y:auto; max-height: 400px"
-          v-if="current_global_instance
-                              && global_attribute_groups_list
-                              && global_attribute_groups_list.length != 0"
-          :mode=" 'annotate' "
+          mode="annotate"
+          key="global_attribute_groups_list"
           :schema_id="schema_id"
           :project_string_id="project_string_id"
           :view_only_mode="view_only_mode"
-          :attribute_group_list_prop = "global_attribute_groups_list"
-          :current_instance = "current_global_instance"
+          :attribute_group_list_prop="global_attribute_groups_list"
+          :current_instance="current_global_instance"
           @attribute_change="$emit('attribute_change', $event)"
-          key="global_attribute_groups_list"
-        >
-        </attribute_group_list>
+        />
       </v-expansion-panel-content>
-
     </v-expansion-panel>
   </v-expansion-panels>
 
 </template>
 
 <script lang="ts">
-
 import attribute_group_list from './attribute_group_list.vue';
+import Vue from "vue"; 
 
-
- import Vue from "vue"; export default Vue.extend( {
-
+export default Vue.extend( {
    name: 'global_attributes_list',
-
    components: {
      attribute_group_list: attribute_group_list
-     },
+    },
     props: {
-      'global_attribute_groups_list' : {
+      global_attribute_groups_list : {
+        type: Array,
         default: null
       },
-      'current_global_instance':{
+      current_global_instance:{
+        type: Object,
         default: null
       },
-      'view_only_mode': {
+      view_only_mode: {
+        type: Boolean,
         default: false
       },
-      'schema_id': {
+      schema_id: {
+        type: Number,
         required: true
       },
-      'project_string_id':{
+      project_string_id:{
+        type: String,
         required: true
+      },
+      open_state: {
+        type: Boolean || undefined,
+        default: undefined
       }
     },
-
     data() {
       return {
-
-        loading: false,
-        error: {},
-        success: false,
         open: undefined
       }
     },
-
     watch: {
-
+      open_state: function(newVal, oldVal) {
+        if (newVal) {
+          this.open = 0
+        } 
+        else {
+          this.open = undefined
+        }
+      }
     },
     mounted() {
-     if(this.global_attribute_groups_list.length > 0 ){
-       this.open = 0
-     }
-    },
-  created() {
-
-
-    },
-    computed: {
-    },
-    methods: {
-
+      if (this.open_state === undefined) {
+        if(this.global_attribute_groups_list.length > 0) this.open = 0
+      } 
+      else {
+        this.open = this.open_state ? 0 : undefined
+      }
     }
   }
-) </script>
+) 
+</script>
 
 <style scoped>
 .sidebar-accordeon-header{
