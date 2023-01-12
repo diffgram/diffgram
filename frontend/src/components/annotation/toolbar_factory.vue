e510827a594f47a55a9868f899e64c1194a3178f
Vitalii Bulyzhyn <vitalybulyzhin@gmail.com>
2023-01-12 11:17:28 -0500

    Create toolbar compoennt

---
+++ frontend/src/components/annotation/toolbar_factory.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Toolbar</h1>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import image_and_video_toolbar from "./image_and_video_annotation/toolbar.vue"
+
+export default Vue.extend({
+  name: "toolbar_factory",
+  components:{
+    image_and_video_toolbar
+  },
+})
+</script>
