7ecdf92c561a37cd7929a0035e37cc404ad2c846
Pablo <pjestrada006@hotmail.com>
2023-02-07 12:57:44 -0600

    fix: layout default columns

--- frontend/src/components/annotation/annotation_ui_factory.vue
+++ frontend/src/components/annotation/annotation_ui_factory.vue
@@ -1686,7 +1686,8 @@ export default Vue.extend({
       file.instance_list = this.annotation_ui_context.compound_global_attributes_instance_list
     },
     set_default_layout_for_child_files: function(child_files){
-      this.annotation_ui_context.panel_settings.set_cols_and_rows_from_total_items(4, child_files.length)
+      let cols = child_files.length < 4 ? child_files.length : 4
+      this.annotation_ui_context.panel_settings.set_cols_and_rows_from_total_items(cols, child_files.length)
     },
     update_root_file: async function (file) {
       if (!file) {
