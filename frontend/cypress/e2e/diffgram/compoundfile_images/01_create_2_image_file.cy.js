e3c2e2c67a7b28afbc5825aeb831f05bba29a02b
Pablo <pjestrada006@hotmail.com>
2023-02-21 12:01:58 -0600

    fix: e2e tests

--- frontend/cypress/e2e/diffgram/compoundfile_images/01_create_2_image_file.cy.js
+++ frontend/cypress/e2e/diffgram/compoundfile_images/01_create_2_image_file.cy.js
@@ -10,7 +10,7 @@ describe('Create 2 image compound file', () => {
       cy.loginByForm(testUser.email, testUser.password)
         .gotToProject(testUser.project_string_id)
         .uploadCompoundFileImages(testUser.project_string_id,  `${uuidv4()}.diffgram`, 2)
-        .wait(3000)
+        .wait(6000)
     })
 
     it('Correctly Renders 2 panels for compound file.', () => {
