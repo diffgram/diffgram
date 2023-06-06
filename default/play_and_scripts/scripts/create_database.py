9c09986774e23ef0f5f1631b8994ab7b537c40dc
Anthony Sarkis <18080164+anthony-sarkis@users.noreply.github.com>
2023-06-05 19:38:17 -0700

    Update create_database.py

--- default/play_and_scripts/scripts/create_database.py
+++ default/play_and_scripts/scripts/create_database.py
@@ -8,13 +8,20 @@ from sqlalchemy import create_engine
 
 
 engine = create_engine(settings.DATABASE_URL)
+
 print(f'Checking DB: {settings.DATABASE_URL}')
+print(f"database_exists(engine.url) {database_exists(engine.url)}")
+
 if not database_exists(engine.url):
     print(f'Creating DB: {settings.DATABASE_URL}')
     from shared.database.core import Base
 
-    create_database(engine.url)
-    Base.metadata.create_all(engine)
-    print('Database created successfully.')
+    try:
+        create_database(engine.url)
+        Base.metadata.create_all(engine)
+        print('Database created successfully.')
+    except exception as e:
+        print(f"{e}")
+        print("Could not create")
 else:
     print('Database already exists.')
