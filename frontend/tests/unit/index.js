1f894f3f2e00fc0d0ca6395b79b1c77773f3e5cc
Zakharchenko Sergey <sergzak022@gmail.com>
2023-08-10 06:13:14 -0700

    revert test related dependencies, update jest config, make unit test pass again

--- frontend/tests/unit/index.js
+++ frontend/tests/unit/index.js
@@ -3,5 +3,9 @@ import Vuetify from "vuetify";
 import 'babel-polyfill';
 import 'jest-canvas-mock';
 
+import main_menu from '../../src/components/main_menu/menu'
+
+Vue.component('main_menu', main_menu)
+
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
