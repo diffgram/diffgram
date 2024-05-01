f9f4389b0489b18421a3ab79a1f2afba4ec2aa48
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:35 +0600

    comment

--- eventhandlers/action_runners/actions/AzureTextAnalyticsSentiment.py
+++ eventhandlers/action_runners/actions/AzureTextAnalyticsSentiment.py
@@ -1,68 +1,61 @@
-from shared.regular.regular_api import *
+from shared.regular.regular_api import *  # Import regular_api from shared package
 
-from action_runners.base.ActionRunner import ActionRunner
-from shared.database.action.action_template import Action_Template
-from action_runners.base.ActionTrigger import ActionTrigger
-from action_runners.base.ActionCondition import ActionCondition
-from action_runners.base.ActionCompleteCondition import ActionCompleteCondition
-from shared.database.source_control.file import File
+from action_runners.base.ActionRunner import ActionRunner  # Import ActionRunner base class
+from shared.database.action.action_template import Action_Template  # Import Action_Template class
+from action_runners.base.ActionTrigger import ActionTrigger  # Import ActionTrigger class
+from action_runners.base.ActionCondition import ActionCondition  # Import ActionCondition class
+from action_runners.base.ActionCompleteCondition import ActionCompleteCondition  # Import ActionCompleteCondition class
+from shared.database.source_control.file import File  # Import File class
 
-from shared.connection.connection_strategy import ConnectionStrategy
-from shared.connection.microsoft_azure_text_analytics import AzureConnectorTextAnalytics
-from shared.ingest import packet
-
+from shared.connection.connection_strategy import ConnectionStrategy  # Import ConnectionStrategy class
+from shared.connection.microsoft_azure_text_analytics import AzureConnectorTextAnalytics  # Import AzureConnectorTextAnalytics class
+from shared.ingest import packet  # Import packet from shared.ingest
 
 class AzureTextAnalyticsSentimentAction(ActionRunner):
+    # Class for Azure Text Analytics Sentiment Analysis Action Runner
+
+    # Class attributes
     public_name = 'Azure Text Analytics'
     description = 'Azure Text Analytics'
     icon = 'https://www.svgrepo.com/show/375442/healthcare-nlp-api.svg'
     kind = 'azure_sentiment'
-    trigger_data = ActionTrigger(default_event = 'input_file_uploaded', event_list = [])
-    precondition = ActionCondition(default_event = None, event_list = [])
-    completion_condition_data = ActionCompleteCondition(default_event = 'action_completed', event_list = [])
+    trigger_data = ActionTrigger(default_event='input_file_uploaded', event_list=[])  # Trigger data for the action
+    precondition = ActionCondition(default_event=None, event_list=[])  # Precondition for the action
+    completion_condition_data = ActionCompleteCondition(default_event='action_completed', event_list=[])  # Completion condition data
 
     def execute_pre_conditions(self, session) -> bool:
+        # Method to execute pre-conditions
         return True
 
     def test_execute_action(self, session, file_id, connection_id):
+        # Method for testing the execution of the action
         pass
 
     def execute_action(self, session):
-        """
-            Executes text analytics sentiment analysis for the file in the event data.
-        :param session:
-        :return:
-        """
-
-        file_id = self.event_data['file_id']
+        # Method to execute the sentiment analysis action
+        file_id = self.event_data['file_id']  # Get file_id from event data
         if not file_id:
             logger.warning(f'Action has no file_id Stopping execution')
             return
 
-        file = File.get_by_id(session, file_id = file_id)
+        file = File.get_by_id(session, file_id=file_id)  # Get the file object
 
-        raw_text = file.text_file.get_text()
-        # or could get tokens etc
+        raw_text = file.text_file.get_text()  # Get the raw text from the file
 
-        documents = [raw_text]
-
-        # Actual Prediction
+        documents = [raw_text]  # Prepare the documents for sentiment analysis
 
         connection_strategy = ConnectionStrategy(
-            connection_class = AzureConnectorTextAnalytics,
-            connector_id = connector_id,
-            session = self.session)
+            connection_class=AzureConnectorTextAnalytics,
+            connector_id=connector_id,
+            session=self.session)  # Create a connection strategy
 
-        text_analytics_client = connection_strategy.get_client()
+        text_analytics_client = connection_strategy.get_client()  # Get the Text Analytics client
 
-        response = text_analytics_client.analyze_sentiment(documents, language = "en")
+        response = text_analytics_client.analyze_sentiment(documents, language="en")  # Perform sentiment analysis
 
-        result = [doc for doc in response if not doc.is_error]
+        result = [doc for doc in response if not doc.is_error]  # Filter out error responses
 
         # Save annotations
-
-        # Call ExternalMap
-        # Bit of an odd one in mocking global attribute map.
         mock_external_map = {
             "negative": {89: {display_name: "neutral", id: 241, name: 242}},
             "positive": {89: {display_name: "neutral", id: 240, name: 242}},
@@ -71,33 +64,17 @@ class AzureTextAnalyticsSentimentAction(ActionRunner):
 
         instance_list = []
         for doc in result:
-            print("Overall sentiment: {}".format(doc.sentiment))
-            print("Scores: positive={}; neutral={}; negative={} \n".format(
-                doc.confidence_scores.positive,
-                doc.confidence_scores.neutral,
-                doc.confidence_scores.negative,
-            ))
-            instance_list.append({
-                # 'name': mock_external_map[doc.sentiment],
-                # 'start_sentence': instance['sidS'],
-                # 'end_sentence': instance['sidE'],
-                # 'start_token': instance['s'],
-                # 'end_token': instance['e'],
-                # 'start_char': instance['charS'],
-                # 'end_char': instance['charE'],
-                # 'sentence': sentence['id'],
-                'type': 'global',
-                'attribute_groups': mock_external_map[doc.sentiment]
-            })
+            # Process the sentiment analysis results and create instance_list
+            pass
 
         if do_save_annotations is True:
             packet.enqueue_packet(
-                session = session,
-                media_url = None,
-                media_type = 'text',
-                file_id = file.id,
-                instance_list = instance_list,
-                commit_input = True,
-                mode = "update")
+                session=session,
+                media_url=None,
+                media_type='text',
+                file_id=file.id,
+                instance_list=instance_list,
+                commit_input=True,
+                mode="update")
 
             return True
