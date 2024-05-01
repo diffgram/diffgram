91450176c69817296ece3bf4964389f1a070cbe6
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:25 +0600

    improve

--- default/methods/annotation/instance_template_list.py
+++ default/methods/annotation/instance_template_list.py
@@ -1,67 +1,58 @@
-try:
-    from methods.regular.regular_api import *
-except:
-    from default.methods.regular.regular_api import *
-from shared.database.annotation.instance_template_relation import InstanceTemplateRelation
+from typing import List, Dict, Union, Optional
+from flask import jsonify, request, Blueprint
+from flask_restplus import Namespace, Resource
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+from shared.database.project import Project
+from shared.database.labels.label_schema import LabelSchema
 from shared.database.annotation.instance_template import InstanceTemplate
 from shared.annotation import Annotation_Update
-from shared.database.labels.label_schema import LabelSchema, LabelSchemaLink
+from shared.database.annotation.instance_template_relation import InstanceTemplateRelation
 
-@routes.route('/api/v1/project/<string:project_string_id>/instance-template/list', methods = ['POST'])
-@Project_permissions.user_has_project(Roles = ["admin", "Editor", "Viewer", "allow_if_project_is_public", "annotator"],
-                                      apis_user_list = ["api_enabled_builder"])
-def list_instance_template_api(project_string_id):
+app = Blueprint('api', __name__)
+api = Namespace('api', description='API namespace')
+
+@app.route('/api/v1/project/<string:project_string_id>/instance-template/list', methods=['POST'])
+@Project_permissions.user_has_project(roles=["admin", "Editor", "Viewer", "allow_if_project_is_public", "annotator"],
+                                      apis_user_list=["api_enabled_builder"])
+class ListInstanceTemplateAPI(Resource):
     """
-        Fetch the list of instance templates in the project.
-    :param project_string_id:
-    :return:
+    Fetch the list of instance templates in the project.
     """
-    instance_template_list_spec_list = [{'schema_id': {'required': True, 'type': int}}]
 
-    log, input, untrusted_input = regular_input.master(
-        request = request,
-        spec_list = instance_template_list_spec_list)
+    @api.expect({'schema_id': {'required': True, 'type': 'integer'}})
+    @api.response(200, 'List of instance templates returned.')
+    @api.response(400, 'Bad request.')
+    def post(self, project_string_id):
+        schema_id = request.json['schema_id']
 
-    if len(log["error"].keys()) >= 1:
-        return jsonify(log = log), 400
+        with Session() as session:
+            project = Project.get_by_string_id(session, project_string_id)
+            instance_template_data, log = self.list_instance_templates_core(session, project, schema_id)
+            if log:
+                return jsonify(log=log), 400
 
-    with sessionMaker.session_scope() as session:
+            return jsonify(instance_template_list=instance_template_data), 200
 
-        project = Project.get_by_string_id(session, project_string_id)
-        new_instance_template_data, log = list_instance_templates_core(
-            session = session,
-            log = log,
-            project = project,
-            schema_id = input['schema_id'],
-        )
-        if len(log["error"].keys()) >= 1:
-            return jsonify(log = log), 400
+    def list_instance_templates_core(self, session: Session, project: Project, schema_id: int) -> Optional[List[Dict]]:
+        """
+        Returns a list of serialized instances templates matching the given project.
+        """
+        success = True
+        log: Dict[str, Union[str, Dict]] = {}
+        schema = LabelSchema.get_by_id(session, schema_id, project.id)
+        if schema.project_id != project.id:
+            log['error'] = {'schema_id': 'Schema does not belong to project'}
+            success = False
+        else:
+            instance_template_list = InstanceTemplate.list(session=session, project=project, schema=schema)
+            if instance_template_list:
+                instance_template_data = [template.serialize(session) for template in instance_template_list]
+            else:
+                instance_template_data = []
+                log['warning'] = {'instance_template_list': 'No instance templates found.'}
+                success = False
 
-        return jsonify(instance_template_list = new_instance_template_data), 200
-
-
-def list_instance_templates_core(session,
-                                 project,
-                                 schema_id,
-                                 log = regular_log.default()):
-    """
-       Returns a list of serialized instances templates matching the given project.
-    :param session:
-    :param log:
-    :param member:
-    :param project:
-    :param instance_list:
-    :return:
-    """
-    result = None
-    schema = LabelSchema.get_by_id(session, schema_id, project.id)
-    if schema.project_id != project.id:
-        log['error']['schema_id'] = 'Schema does not belong to project'
-        return None, log
-    instance_template_list = InstanceTemplate.list(
-        session = session,
-        project = project,
-        schema = schema
-    )
-    result = [template.serialize(session) for template in instance_template_list]
-    return result, log
+        if not success:
+            return None, log
+        return instance_template_data, log
