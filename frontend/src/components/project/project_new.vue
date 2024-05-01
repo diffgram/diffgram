1cf6046a2465bf9a59c362d050bb5c49668af612
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:57 +0600

    comment

--- frontend/src/components/project/project_new.vue
+++ frontend/src/components/project/project_new.vue
@@ -1,98 +1,103 @@
 <template>
+  <!-- Display a success alert upon enabling the builder API -->
+  <v-container>
+    <v-alert type="success" v-if="builder_api_enabled_success"
+      >Success! The first step to get started is to create a project.</v-alert
+    >
+    <!-- A card for creating a new project -->
+    <v-card elevation="0">
+      <v-card-title>
+        <h1 class="headline">Name Your New Project</h1>
+      </v-card-title>
 
-   <v-container>
-      <v-alert type="success"
-               :value="builder_api_enabled_success">
-        Success! The first step to get started is to create a project.
-      </v-alert>
-      <v-card elevation="0">
-        <v-card-title>
-          <h1 class="headline">Name Your New Project</h1>
-        </v-card-title>
+      <v-card-text
+        >A project is a big deal. It has many Datasets, Users, Tasks, Schema and
+        more.</v-card-text
+      >
 
+      <!-- Container for the form fields -->
+      <v-container>
+        <!-- Project name input field -->
+        <v-text-field
+          label="Name"
+          data-cy="project_name"
+          v-model="name"
+          :rules="[rules.required, rules.name]"
+        >
+        </v-text-field>
+
+        <!-- Project goal input field -->
+        <v-text-field
+          label="Goal"
+          data-cy="project_goal"
+          v-model="goal"
+          placeholder="What are you trying to achieve?"
+          hint="ie training data for xyz, or build a brain to detect zyx"
+        >
+        </v-text-field>
+
+        <!-- Organization input field (read-only) -->
+        <v-text-field
+          v-if="$store.state.org.current.id"
+          data-cy="organization"
+          label="Org"
+          disabled
+          v-model="$store.state.org.current.name"
+        >
+        </v-text-field>
+
+        <!-- Display the project ID -->
         <v-card-text>
-          A project is a big deal.
-          It has many Datasets, Users, Tasks, Schema and more.
+          {{ project_string_id_text }}
         </v-card-text>
 
-        <v-container>
-          <v-text-field label="Name"
-                        data-cy="project_name"
-                        v-model="name"
-                        :rules="[rules.name]">
-          </v-text-field>
+        <!-- Create project button -->
+        <v-btn
+          color="success"
+          data-cy="create_project_button"
+          :loading="loading"
+          x-large
+          @click.native="loader = 'loading'"
+          @click="new_project"
+          :disabled="loading"
+          >Create</v-btn
+        >
 
-          <v-text-field label="Goal"
-                        data-cy="project_goal"
-                        v-model="goal"
-                        placeholder="What are you trying to achieve?"
-                        hint="ie training data for xyz, or build a brain to detect zyx"
-                        >
-          </v-text-field>
+        <!-- Error display component -->
+        <v_error_multiple :error="error"></v_error_multiple>
+      </v-container>
+    </v-card>
 
-          <v-text-field v-if="$store.state.org.current.id"
-                        data-cy="organization"
-                        label="Org"
-                        disabled
-                        v-model="$store.state.org.current.name">
-          </v-text-field>
-
-          <v-card-text>
-            {{ project_string_id_text }}
-          </v-card-text>
-
-          <v-btn  color="success"
-                  data-cy="create_project_button"
-                  :loading="loading"
-                  x-large
-                  @click.native="loader = 'loading'"
-                  @click="new_project"
-                  :disabled="loading">
-            Create
-          </v-btn>
-
-          <v_error_multiple :error="error">
-          </v_error_multiple>
-
-        </v-container>
-
+    <!-- Verify email alert -->
+    <div class="pt-2">
+      <v-card>
+        <v-alert type="info" data-cy="verify_email_alert" outlined
+          >Please verify your email.</v-alert
+        >
       </v-card>
-
-      <div class="pt-2">
-        <v-card>
-          <v-alert type="info"
-                   data-cy="verify_email_alert"
-                   outlined
-                   :value="builder_api_enabled_success">
-            Please verify your email.
-          </v-alert>
-        </v-card>
-      </div>
-
-    </v-container>
-
+    </div>
+  </v-container>
 </template>
 
 <script lang="ts">
-
-import axios from '../../services/customInstance';
-import sillyname from 'sillyname';
+import axios from "../../services/customInstance";
+import sillyname from "sillyname";
 import { getProjectList } from "../../services/projectServices";
 
-import Vue from "vue"; export default Vue.extend( {
+import Vue from "vue"; export default Vue.extend({
   name: 'new_project',
   data() {
     return {
-      loading: false,
+      loading: false, // Loading state for the create project button
 
-      name: sillyname().split(" ")[0],
-      goal: null,
+      name: sillyname().split(" ")[0], // Project name
+      goal: null, // Project goal
 
-      builder_api_enabled_success: false,
+      builder_api_enabled_success: false, // Success state for enabling the builder API
 
-      error: {},
+      error: {}, // Error object for displaying errors
 
-      rules: {
+      rules: { // Validation rules for form fields
         required: (value) => !!value || 'Required.',
         name: (value) => {
           const pattern = new RegExp("^[a-zA-Z0-9_ ]{4,30}$")
@@ -102,10 +107,10 @@ import Vue from "vue"; export default Vue.extend( {
     }
   },
   computed: {
-    project_string_id: function () {
+    project_string_id: function () { // Generate a string ID from the project name
       return this.name.replace(/\s+/g, '-').toLowerCase()
     },
-    project_string_id_text: function () {
+    project_string_id_text: function () { // Display the generated string ID
       if (this.name != null) {
         return "Your project ID will be: " + this.project_string_id
       } else {
@@ -117,11 +122,11 @@ import Vue from "vue"; export default Vue.extend( {
     this.builder_api_enabled_success = (this.$route.query["builder_api_enabled_success"] == 'true')
   },
   methods: {
-    new_project: function () {
+    new_project: function () { // Create a new project
 
-      this.loading = true;
+      this.loading = true; // Set loading state to true
 
-      this.error = {}
+      this.error = {} // Reset error object
 
       axios.post('/api/project/new',
         {
@@ -134,23 +139,4 @@ import Vue from "vue"; export default Vue.extend( {
       }).then(async response => {
         if (response.data.log.success == true) {
 
-          this.$store.commit('set_project', response.data.project)
-
-          this.$emit('project_created', response.data.project, response.data.schema);
-          const projext_list_response = await getProjectList();
-          const project_list = projext_list_response.data.project_list;
-          this.$store.commit("set_userProjects_list", project_list);
-        } else {
-
-          this.loading = false
-        }
-      })
-      .catch(error => {
-        this.error = error.response.data.log.error,
-        this.loading = false
-      });
-    }
-  }
-}
-
-) </script>
+          this.$store.commit('set_project', response.data.project) // Set the
