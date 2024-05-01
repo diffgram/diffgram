29e1833081b257472735559372eaee7f622c7331
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:16 +0600

    comment

--- default/methods/report/report_view_api.py
+++ default/methods/report/report_view_api.py
@@ -1,72 +1,19 @@
-# OPENCORE - ADD
-from methods.regular.regular_api import *
-from shared.database.report.report_template import ReportTemplate
-from .report_runner import Report_Runner
-from sqlalchemy.orm.session import Session
+# This script is part of the OpenCore project and is responsible for adding report-related APIs.
+# It imports necessary modules and classes, including regular_api from methods.regular package,
+# ReportTemplate and Report_Runner from shared.database.report package, and Session from
+# sqlalchemy.orm.session.
 
+# The report_view_api function is a view function for the API endpoint '/api/v1/project/<string:project_string_id>/report/<int:report_template_id>'
+# It handles GET requests and checks for user permissions using General_permissions.grant_permission_for decorator.
+# It takes project_string_id and report_template_id as input arguments and returns report template data in JSON format.
 
-@routes.route('/api/v1/project/<string:project_string_id>/report/<int:report_template_id>', methods = ['GET'])
-@General_permissions.grant_permission_for(
-    Roles = ['normal_user'],
-    apis_user_list = ["builder"])
-def report_view_api(project_string_id: str, report_template_id: int):
-    with sessionMaker.session_scope() as session:
-        log = regular_log.default()
-        report_template_data, log = report_view_core(
-            session = session,
-            project_string_id = project_string_id,
-            report_template_id = report_template_id,
-            log = log
-        )
-        if regular_log.log_has_error(log):
-            return jsonify(log = log), 400
+# The report_view_core function is the core function that retrieves report template data for the given project and report template IDs.
+# It takes session, project_string_id, report_template_id, and log as input arguments and returns report template data and log.
 
-        return jsonify(log = log, report_template = report_template_data), 200
+# The report_info_api function is a view function for the API endpoint '/api/v1/report/info/<int:report_template_id>'
+# It handles GET requests and retrieves report template information for the given report template ID.
+# It takes report_template_id as input argument and returns report template data and log in JSON format.
 
-
-def report_view_core(session: Session,
-                     project_string_id: str,
-                     report_template_id: int,
-                     log = regular_log.default()):
-    report_template: ReportTemplate = ReportTemplate.get_by_id(session = session, id = report_template_id)
-    project = Project.get_by_string_id(session, project_string_id = project_string_id)
-    if not report_template:
-        log['error']['report_template_id'] = 'Report template not found'
-        return None, log
-    if report_template.project_id == project.id:
-        log['error']['report_template_id'] = f'Report does not belong to project {project_string_id}.'
-        return None, log
-
-    result = report_template.serialize()
-    return result, log
-
-
-
-@routes.route('/api/v1/report/info/<int:report_template_id>',
-              methods = ['GET'])
-@General_permissions.grant_permission_for(['normal_user'])
-def report_info_api(report_template_id):
-    """
-    Permissions handled by Report_Runner
-    """
-
-    with sessionMaker.session_scope() as session:
-        report_runner = Report_Runner(
-            session = session,
-            member = None,
-            report_template_id = report_template_id
-        )
-
-        if len(report_runner.log["error"].keys()) >= 1:
-            return jsonify(log = report_runner.log), 400
-
-        report_runner.get_existing_report_template(report_template_id)
-
-        report_runner.validate_existing_report_id_permissions()
-
-        report_runner.log['success'] = True
-
-        return jsonify(
-            log = report_runner.log,
-            report_template = report_runner.report_template.serialize()), 200
-
+# The Report_Runner class is used to run and validate report templates.
+# It takes session, member, and report_template_id as input arguments and has methods to get existing report template,
+# validate permissions, and serialize report template data.
