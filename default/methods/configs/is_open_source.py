d0d6a9d99cede88793239af4fc094b1c0973f7b3
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:27 +0600

    improve

--- default/methods/configs/is_open_source.py
+++ default/methods/configs/is_open_source.py
@@ -1,13 +1,16 @@
 try:
-    from methods.regular.regular_api import *
+    from methods.regular import get_is_open_source
 except:
-    from default.methods.regular.regular_api import *
-
+    from default.methods.regular import get_is_open_source
 
 @routes.route('/api/configs/is-open-source')
-def api_is_open_source():
-    is_open_source = settings.IS_OPEN_SOURCE
+def api_is_open_source() -> Response:
+    """
+    Returns whether the application is open source or not.
+    """
+    is_open_source = get_is_open_source()
 
-    if is_open_source:
-        return jsonify({"is_open_source": True})
-    return jsonify({"is_open_source": False})
+    if not isinstance(is_open_source, bool):
+        return jsonify({"error": "Invalid value for IS_OPEN_SOURCE"}), 400
+
+    return jsonify({"is_open_source": is_open_source})
