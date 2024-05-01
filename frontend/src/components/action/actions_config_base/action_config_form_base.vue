5d675f267b1c5783120a73c9aabbb525aea6c474
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:47 +0600

    comment

--- frontend/src/components/action/actions_config_base/action_config_form_base.vue
+++ frontend/src/components/action/actions_config_base/action_config_form_base.vue
@@ -1,6 +1,8 @@
 <template>
+  <!-- Container for the entire component -->
   <v-container fluid>
 
+    <!-- Slot for trigger configuration with props and methods -->
     <slot name="triggers" :action_template="action_template">
       <trigger_config
         :action_template="action_template"
@@ -8,6 +10,8 @@
         :actions_list=actions_list
         :action="action"></trigger_config>
     </slot>
+
+    <!-- Slot for pre-condition configuration with props and methods -->
     <slot name="pre_conditions">
       <pre_conditions_config
         :action_template="action_template"
@@ -17,9 +21,13 @@
 
       </pre_conditions_config>
     </slot>
+
+    <!-- Slot for action configuration -->
     <slot name="action_config">
 
     </slot>
+
+    <!-- Slot for completion trigger configuration with props and methods -->
     <slot name="completion_trigger">
       <complete_conditions_config
         :project_string_id="project_string_id"
@@ -38,6 +46,7 @@ import complete_conditions_config from './complete_conditions_config.vue'
 import pre_conditions_config from './pre_conditions_config.vue'
 import trigger_config from './trigger_config.vue'
 
+// Component registration and props definition
 export default Vue.extend({
     name: 'action_config_wizard_base',
     components: {
@@ -54,15 +63,15 @@ export default Vue.extend({
     ],
     data() {
       return {
-        is_open: true,
-        search: ''
+        is_open: true, // Indicates if the component is open or not
+        search: '' // Search input value
       }
     },
   }
 ) </script>
 
-
 <style>
+// Style for the code elements
 code {
   width: 100%;
   height: 100% !important;
