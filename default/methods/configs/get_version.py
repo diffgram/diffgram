43caace09c0926270982639e27f0f64a865eddf5
jaiwiwjwjwisn <168209237+jaiwiwjwjwisn@users.noreply.github.com>
2024-05-02 00:28:11 +0600

    comment

--- default/methods/configs/get_version.py
+++ default/methods/configs/get_version.py
@@ -1,11 +1,18 @@
 try:
+    # Import the regular API from the methods.regular.regular_api module,
+    # or use the default implementation if there is an exception.
     from methods.regular.regular_api import *
 except:
+    # Import the regular API from the default.methods.regular.regular_api module.
     from default.methods.regular.regular_api import *
 
 
+# Define a new route for the Flask application at the URL '/api/configs/version'
+# that returns the version of the Diffgram API as a JSON response.
 @routes.route('/api/configs/version')
 def api_get_version():
+    # Get the version of the Diffgram API from the settings module.
     version = settings.DIFFGRAM_VERSION_TAG
 
+    # Return a JSON response with the version number.
     return jsonify({"version": version})
